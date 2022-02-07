abfdde77ff917e230591e734f8e43f31e7ffa229
Maxwell Plotkin <maxwell.a.plotkin@jpl.nasa.gov>
2022-02-07 14:31:00 -0800

    Remove redirect to stderr in subscriber wrapper script

--- tools/daac_data_subscriber.sh
+++ tools/daac_data_subscriber.sh
@@ -17,16 +17,16 @@ export LD_LIBRARY_PATH=/opt/conda/lib:$LD_LIBRARY_PATH
 # source environment
 source $HOME/verdi/bin/activate
 
-echo "##########################################" 1>&2
+echo "##########################################"
 echo "Installing dependencies"
-pip install .
+pip install $BASE_PATH
 echo "Running job to subscribe to data"
-date 1>&2
+date
 echo "Running python code: daac_data_subscriber.py"
 python $BASE_PATH/daac_data_subscriber.py -m 10 -c HLSS30 -s "opera-dev-isl-fwd-mplotkin" --extensions ".tif" > daac_data_subscriber.log 2>&1
 STATUS=$?
-echo "Finished running daac_data_subscriber.py: " 1>&2
-date 1>&2
+echo "Finished running daac_data_subscriber.py"
+date
 if [ $STATUS -ne 0 ]; then
   echo "Failed to run daac_data_subscriber.py" 1>&2
   cat daac_data_subscriber.log 1>&2
