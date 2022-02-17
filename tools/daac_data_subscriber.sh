dbd70c49b4bbcf4191a34ed5e5b2a59f2e54c4ff
Maxwell Plotkin <maxwell.a.plotkin@jpl.nasa.gov>
2022-02-16 16:07:20 -0800

    More demo mods

--- tools/daac_data_subscriber.sh
+++ tools/daac_data_subscriber.sh
@@ -24,7 +24,7 @@ echo "Running python code: daac_data_subscriber.py"
 # Forward processing use case; query previous 10 minutes
 # python $BASE_PATH/daac_data_subscriber.py -m 10 -c HLSS30 -s "opera-dev-isl-fwd-mplotkin" --extensions ".tif" > daac_data_subscriber.log 2>&1
 # Demo use case; query specific static range
-python $BASE_PATH/daac_data_subscriber.py -c HLSS30 -s "opera-dev-isl-fwd-mplotkin" --extensions ".tif" -sd 2021-09-07T16:39:00Z -ed 2021-09-07T16:40:00Z > daac_data_subscriber.log 2>&1
+python $BASE_PATH/daac_data_subscriber.py -c HLSS30 -s "opera-dev-isl-fwd-mplotkin" --extensions ".tif" -sd 2018-12-31T00:00:00Z -ed 2019-01-01T00:00:00Z > daac_data_subscriber.log 2>&1
 STATUS=$?
 echo "Finished running daac_data_subscriber.py"
 date
