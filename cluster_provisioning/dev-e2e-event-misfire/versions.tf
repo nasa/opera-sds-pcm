101f6a4edc93897667e1317c1a1079a0d0da07ec
hyunlee <Hyun.H.Lee@jpl.nasa.gov>
2022-04-07 19:40:18 -0700

    Change back terraform version >=0.13 until we upgrade terraform version on opera-pcm-ci

--- cluster_provisioning/dev-e2e-event-misfire/versions.tf
+++ cluster_provisioning/dev-e2e-event-misfire/versions.tf
@@ -1,13 +1,1 @@
-
-terraform {
-  required_version = ">= 1.0.11"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "2.70.1"
-    }
-    null = {
-      source = "hashicorp/null"
-    }
-  }
-}
+../modules/common/versions.tf
--- cluster_provisioning/modules/common/main.tf
+++ cluster_provisioning/modules/common/main.tf
@@ -1950,7 +1950,6 @@ resource "aws_lambda_permission" "event-misfire_lambda" {
 #    variables = {
 #      "MOZART_URL": "https://${aws_instance.mozart.private_ip}/mozart",
 #      "JOB_QUEUE": "${var.project}-job_worker-small",
-#      #"JOB_QUEUE": "opera-job_worker-small",
 #      "JOB_TYPE": local.accountability_report_job_type,
 #      "JOB_RELEASE": var.pcm_branch,
 #      "REPORT_NAME": "ObservationAccountabilityReport",
@@ -2008,7 +2007,7 @@ resource "aws_lambda_function" "data_subscriber_download_timer" {
   environment {
     variables = {
       "MOZART_URL": "https://${aws_instance.mozart.private_ip}/mozart",
-      "JOB_QUEUE": "opera-job_worker-small",
+	  "JOB_QUEUE": "${var.project}-job_worker-small",
       "JOB_TYPE": local.data_download_job_type,
       "JOB_RELEASE": var.pcm_branch,
       "ISL_BUCKET_NAME": local.isl_bucket,
