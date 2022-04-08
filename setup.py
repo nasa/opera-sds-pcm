c3bea741fd6e8916eb4ff7ef78626412399f62d1
chrisjrd <chris.jrd@gmail.com>
2022-04-08 13:53:20 -0700

    Revert "feat(test): migrate to pyproject.toml"
    This reverts commit 86e107260d8e6bfd291ab3b3ca9a2f032f778136.

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup, find_packages
 
+# adaptation_path = "folder/"
+
 setup(
+    name='opera_pcm',
+    version='1.0.0',
     packages=find_packages(),
+    install_requires=[
+        'smart_open',
+        'pandas',
+        'h5py'
+    ]
 )
