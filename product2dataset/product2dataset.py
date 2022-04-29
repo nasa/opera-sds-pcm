a1b2040d986c7ce4f174c821f06d773846fccbf4
Scott Collins <collinss@jpl.nasa.gov>
2022-04-29 08:54:09 -0700

    Updated product2dataset.py to merge all extracted .met.json files into a single one for the whole dataset

--- product2dataset/product2dataset.py
+++ product2dataset/product2dataset.py
@@ -7,6 +7,8 @@ Adapted for OPERA PCM by Scott Collins
 """
 from __future__ import print_function
 
+import glob
+import json
 import os
 import sys
 import shutil
@@ -18,7 +20,7 @@ from commons.logger import logger
 
 from extractor import extract
 from util.conf_util import SettingsConf, PGEOutputsConf
-from util.checksum_util import create_dataset_checksums
+#from util.checksum_util import create_dataset_checksums
 
 PRIMARY_KEY = "Primary"
 SECONDARY_KEY = "Secondary"
@@ -29,7 +31,7 @@ DATASETS_DIR_NAME = "datasets"
 
 def convert(product_dir, pge_name, rc_file=None, pge_output_conf_file=None,
             settings_conf_file=None, extra_met=None):
-    created_datasets = []
+    created_datasets = set()
 
     pge_outputs_cfg = PGEOutputsConf(pge_output_conf_file).cfg
     pge_config = pge_outputs_cfg[pge_name]
@@ -64,18 +66,54 @@ def convert(product_dir, pge_name, rc_file=None, pge_output_conf_file=None,
 
             if hashcheck:
                 hash_algo = products[output_type][product].get("hash_algo", DEFAULT_HASH_ALGO)
-                create_dataset_checksums(os.path.join(dataset_dir, product), hash_algo)
+                #create_dataset_checksums(os.path.join(dataset_dir, product), hash_algo)
 
-            created_datasets.append(dataset_dir)
-
-    # Rename RunConfig to its dataset
-    if rc_file:
-        renamed_rc_file = os.path.join(product_dir, f"{os.path.basename(created_datasets[0])}.rc.yaml")
-        logger.info(f"Copying RunConfig file to {renamed_rc_file}")
-        shutil.copyfile(rc_file, renamed_rc_file)
-        products[SECONDARY_KEY][os.path.basename(renamed_rc_file)] = {"hashcheck": False}
+            created_datasets.add(dataset_dir)
 
     for dataset in created_datasets:
+        dataset_id = dataset.split(os.sep)[-1]
+
+        # Merge all created .met.json files into a single one for use with accountability reporting
+        dataset_met_json = {"Files": []}
+        combined_file_size = 0
+
+        for met_json_file in glob.iglob(os.path.join(dataset, '*.met.json')):
+            with open(met_json_file, 'r') as infile:
+                met_json = json.load(infile)
+                file_key = os.path.splitext(met_json["FileName"])[0]
+                combined_file_size += int(met_json["FileSize"])
+
+                # Extract a copy of the "Product*" key/values to include at the top level
+                # They should be the same values for each file in the dataset
+                product_keys = list(filter(lambda key: key.startswith("Product"), met_json.keys()))
+
+                for product_key in product_keys:
+                    extra_met[product_key] = met_json[product_key]
+                    met_json.pop(product_key)
+
+                dataset_met_json["Files"].append(met_json)
+
+            # Remove the individual .met.json files after they've been merged
+            os.unlink(met_json_file)
+
+        # Add fields to the top-level of the .met.json file
+        dataset_met_json["FileSize"] = combined_file_size
+        dataset_met_json["FileName"] = dataset_id
+        dataset_met_json.update(extra_met)
+
+        dataset_met_json_path = os.path.join(dataset, f"{dataset_id}.met.json")
+
+        logger.info(f"Creating combined dataset metadata file {dataset_met_json_path}")
+        with open(dataset_met_json_path, 'w') as outfile:
+            json.dump(dataset_met_json, outfile, indent=2)
+
+        # Rename RunConfig to its dataset
+        if rc_file:
+            renamed_rc_file = os.path.join(product_dir, f"{os.path.basename(dataset)}.rc.yaml")
+            logger.info(f"Copying RunConfig file to {renamed_rc_file}")
+            shutil.copyfile(rc_file, renamed_rc_file)
+            products[SECONDARY_KEY][os.path.basename(renamed_rc_file)] = {"hashcheck": False}
+
         for secondary_product in products[SECONDARY_KEY].keys():
             source = os.path.join(product_dir, secondary_product)
             target = os.path.join(dataset, secondary_product)
@@ -86,9 +124,9 @@ def convert(product_dir, pge_name, rc_file=None, pge_output_conf_file=None,
 
             if hashcheck:
                 hash_algo = products[SECONDARY_KEY][secondary_product].get("hash_algo", DEFAULT_HASH_ALGO)
-                create_dataset_checksums(target, hash_algo)
+                #create_dataset_checksums(target, hash_algo)
 
-    return created_datasets
+    return list(created_datasets)
 
 
 def get_patterns(pattern_obj_array):
