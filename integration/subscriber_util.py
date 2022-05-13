7d856c2bfed9f0f317c6412183dbf44df24d0a40
chrisjrd <chris.jrd@gmail.com>
2022-05-13 14:06:40 -0700

    feat(data-subscriber): minor refactor
    Refs #85

--- integration/subscriber_util.py
+++ integration/subscriber_util.py
@@ -23,23 +23,9 @@ aws_lambda: LambdaClient = boto3.client("lambda")
 def invoke_subscriber_query_lambda():
     logging.info("Invoking data subscriber query timer lambda")
 
-    dummy_payload_cloudwatch_scheduled_event = b"""
-        {
-          "id": "cdc73f9d-aea9-11e3-9d5a-835b769c0d9c",
-          "detail-type": "Scheduled Event",
-          "source": "aws.events",
-          "account": "123456789012",
-          "time": "1970-01-01T00:00:00Z",
-          "region": "us-east-1",
-          "resources": [
-            "arn:aws:events:us-east-1:123456789012:rule/ExampleRule"
-          ],
-          "detail": {}
-        }
-    """
     response: mypy_boto3_lambda.type_defs.InvocationResponseTypeDef = aws_lambda.invoke(
         FunctionName=config["DATA_SUBSCRIBER_QUERY_LAMBDA"],
-        Payload=dummy_payload_cloudwatch_scheduled_event
+        Payload=generate_dummy_payload_cloudwatch_scheduled_event()
     )
     return response
 
@@ -47,7 +33,7 @@ def invoke_subscriber_query_lambda():
 @backoff.on_predicate(
     backoff.constant,
     lambda job_status: job_status["success"] is not True or job_status["status"] != "job-completed",
-    max_time=60*1,
+    max_time=60 * 1,
     on_success=success_handler,
     on_giveup=lambda _: raise_(Exception()),
     interval=30,
@@ -67,7 +53,7 @@ def wait_for_query_job(job_id):
 @backoff.on_predicate(
     backoff.constant,
     lambda is_truthy_status: not is_truthy_status,
-    max_time=60*10,
+    max_time=60 * 10,
     on_success=success_handler,
     on_giveup=lambda _: raise_(Exception()),
     interval=30,
@@ -75,7 +61,8 @@ def wait_for_query_job(job_id):
 def wait_for_download_jobs(job_id):
     logging.info(f"Checking download job status. {job_id=}")
 
-    response: elasticsearch_dsl.response.Response = Search(using=get_es_client(), index="data_subscriber_product_catalog") \
+    response: elasticsearch_dsl.response.Response = Search(using=get_es_client(),
+                                                           index="data_subscriber_product_catalog") \
         .query("match", query_job_id=job_id) \
         .query("match", downloaded=True) \
         .execute()
@@ -87,3 +74,20 @@ def wait_for_download_jobs(job_id):
     for hit in response:
         logging.info(f"{hit=}")
     return True
+
+
+def generate_dummy_payload_cloudwatch_scheduled_event():
+    return b"""
+        {
+          "id": "cdc73f9d-aea9-11e3-9d5a-835b769c0d9c",
+          "detail-type": "Scheduled Event",
+          "source": "aws.events",
+          "account": "123456789012",
+          "time": "1970-01-01T00:00:00Z",
+          "region": "us-east-1",
+          "resources": [
+            "arn:aws:events:us-east-1:123456789012:rule/ExampleRule"
+          ],
+          "detail": {}
+        }
+    """
