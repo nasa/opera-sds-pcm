0c3f5586ab7b7194860bdc63bd3ec67c54a779da
chrisjrd <chris.jrd@gmail.com>
2022-05-17 17:03:51 -0700

    feat(data-subscriber): sync with s30 wrapper script
    Refs #85

--- data_subscriber/hls/hlsl30_query_minutes.sh
+++ data_subscriber/hls/hlsl30_query_minutes.sh
@@ -5,32 +5,6 @@ echo "args: $*"
 BASE_PATH=$(dirname "${BASH_SOURCE}")
 BASE_PATH=$(cd "${BASE_PATH}"; pwd)
 
-MINUTES=$1
-PROVIDER=$2
-
-if [ ! -z "$3" ]; then
-  BOUNDING_BOX="-b ${3}"
-fi
-DOWNLOAD_JOB_RELEASE="${4:=issue_85}"
-DOWNLOAD_JOB_QUEUE="${5}"
-ISL_BUCKET_NAME="${6}"
-CHUNK_SIZE="${7}"
-CHUNK_SIZE="${CHUNK_SIZE:=2}"
-
-SMOKE_RUN="${8:=false}"
-if [ $SMOKE_RUN = "true" ]; then
-  SMOKE_RUN="--smoke-run"
-else
-  SMOKE_RUN=""
-fi
-
-DRY_RUN="${9:=false}"
-if [ $DRY_RUN = "true" ]; then
-  DRY_RUN="--dry-run"
-else
-  DRY_RUN=""
-fi
-
 # source PGE env
 export OPERA_HOME=/home/ops/verdi/ops/opera-pcm
 export PYTHONPATH=$BASE_PATH:$OPERA_HOME:$PYTHONPATH
@@ -46,18 +20,9 @@ echo "Running job to query LPDAAC HLSL30 data"
 date
 
 # Forward processing use case; query previous 60 minutes
-echo "python $OPERA_HOME/data_subscriber/daac_data_subscriber.py query -m $MINUTES -p $PROVIDER -c HLSL30 $BOUNDING_BOX"
 python $OPERA_HOME/data_subscriber/daac_data_subscriber.py query \
--m $MINUTES \
--p $PROVIDER \
 -c HLSL30 \
-$BOUNDING_BOX \
---release-version=$DOWNLOAD_JOB_RELEASE \
---job-queue=$DOWNLOAD_JOB_QUEUE \
---isl-bucket=$ISL_BUCKET_NAME \
---chunk-size=$CHUNK_SIZE \
-$SMOKE_RUN \
-$DRY_RUN \
+$* \
 2>&1
 
 if [ $? -eq 0 ]; then
