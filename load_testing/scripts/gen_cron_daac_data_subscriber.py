04d8201951db5f59d2d238084d20ac69778d1617
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2022-06-01 21:25:52 -0700

    Updated gen_cron_daac script to work with the latest daac_data_subscriber script

--- load_testing/scripts/gen_cron_daac_data_subscriber.py
+++ load_testing/scripts/gen_cron_daac_data_subscriber.py
@@ -18,25 +18,18 @@ import argparse
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="Verbose mode.")
-
-    parser.add_argument("-c", "--collection", dest="collection", required=True,
-                             help="The collection for which you want to retrieve data.")
-    parser.add_argument("-sd", "--start-date", dest="start_date", default=False,
+    parser.add_argument("-m", "--mode", dest="mode", type=str, required=True,
+                             help="'full', 'query', or 'download' See daac_data_subscriber help for more info.")
+    parser.add_argument("-s", "--start-date", dest="start_date", default=False,
                              help="The ISO date time after which data should be retrieved. For Example, --start-date 2021-01-14T00:00:00Z")
-    parser.add_argument("-b", "--bounds", dest="bbox", 
-                             help="The bounding rectangle to filter result in. Format is W Longitude,S Latitude,E Longitude,N Latitude without spaces. Due to an issue with parsing arguments, to use this command, please use the -b=\"-180,-90,180,90\" syntax when calling from the command line. Default: \"-180,-90,180,90\".")
     parser.add_argument("-jp", "--job-period", dest="job_period", type=int, default=60,
                              help="How often, in minutes, should the cron job run (default: 60 minutes).")
     parser.add_argument("-jc", "--job-count", dest="job_count", type=int, required=True,
                              help="How many cron jobs to run.")
     parser.add_argument("-mult", "--multiplier", dest="multiplier", type=float, default=1.0,
                              help="Floating point multiplier factor for the time window. Each line item in the crontab runs every hour. If the multiplier is 1, each cronjob will cover 1 hour worth of data. If it's 0.5, it will cover 30 mins-worth. If it's 2 it will cover 2-hr worth. There is no overlap in data being fetched; the time-covered for the data will span NUM_HOURS * MULTIPLIER (default: 1.0).")
-    parser.add_argument("-p", "--provider", dest="provider", default='LPCLOUD',
-                             help="Specify a provider for collection search. Default is LPCLOUD.")
-    parser.add_argument("-s", "--s3bucket", dest="s3_bucket", required=True,
-                             help="The s3 bucket where data products will be downloaded.")
-    parser.add_argument("-x", "--transfer-protocol", dest="transfer_protocol", default='s3',
-                             help="The protocol used for retrieving data, HTTPS or default of S3")
+    parser.add_argument("-pass", "--pass-through", dest="pass_through", type=str, required=False,
+                             help="Options to pass through to the daac_data_subscriber script directly")
 
     return parser
 
@@ -60,8 +53,7 @@ for i in range(0, args.job_count):
     stop = start_dt + timedelta(minutes=(i+1)*increment_mins)
     stop_str = stop.strftime(date_format_str)
     cron = f"{job_dt.minute} {job_dt.hour} {job_dt.day} {job_dt.month} *"
-    print(f" {cron} {_python_and_subs} -sd {start_str} -ed {stop_str} -c {args.collection} -s {args.s3_bucket}")
+    print(f" {cron} {_python_and_subs} {args.mode} -s {start_str} -e {stop_str} {args.pass_through}")
 
     job_dt = job_dt + timedelta(minutes = args.job_period)
-
 
