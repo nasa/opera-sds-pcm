54e7b7f693a2fd71240e4840ffc89bbbc6ddf034
chrisjrd <chris.jrd@gmail.com>
2022-06-10 13:44:30 -0700

    feat(system): updated AMIs and CI machine host IP

--- cluster_provisioning/dev-e2e-baseline-pge/variables.tf
+++ cluster_provisioning/dev-e2e-baseline-pge/variables.tf
@@ -232,8 +232,8 @@ variable "common_ci" {
   type = map(string)
   default = {
     name       = "ci"
-    private_ip = "100.104.40.248"
-    public_ip  = "100.104.40.248"
+    private_ip = "100.104.41.51"
+    public_ip  = "100.104.41.51"
   }
 }
 
