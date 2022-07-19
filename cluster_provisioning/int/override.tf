a047593b38ea95ebc7328a48cc4f30b55417a3ef
hyunlee <Hyun.H.Lee@jpl.nasa.gov>
2022-07-19 16:53:42 -0700

    - Add world cover stage fix: issue_147 - Change branch version for int and dev-int from 1.0.0-rc.1.0 to 1.0.0-rc.2.0

--- cluster_provisioning/int/override.tf
+++ cluster_provisioning/int/override.tf
@@ -183,27 +183,27 @@ variable "hysds_release" {
 }
 
 variable "lambda_package_release" {
-  default = "1.0.0-rc.1.0"
+  default = "1.0.0-rc.2.0"
 }
 
 variable "pcm_commons_branch" {
-  default = "1.0.0-rc.1.0"
+  default = "1.0.0-rc.2.0"
 }
 
 variable "pcm_branch" {
-  default = "1.0.0-rc.1.0"
+  default = "1.0.0-rc.2.0"
 }
 
 variable "product_delivery_branch" {
-  default = "1.0.0-rc.1.0"
+  default = "1.0.0-rc.2.0"
 }
 
 variable "bach_api_branch" {
-  default = "1.0.0-rc.1.0"
+  default = "1.0.0-rc.2.0"
 }
 
 variable "bach_ui_branch" {
-  default = "1.0.0-rc.1.0"
+  default = "1.0.0-rc.2.0"
 }
 
 ###### Roles ########
