84a239d00e95a820d6b7c5e46d67cc222b4ba410
Maxwell Plotkin <maxwell.a.plotkin@jpl.nasa.gov>
2022-09-30 12:39:36 -0700

    Add temporal logic to slc catalog

--- data_subscriber/slc/slc_catalog.py
+++ data_subscriber/slc/slc_catalog.py
@@ -31,7 +31,7 @@ class SLCProductCatalog(ElasticsearchUtility):
                                      "mappings": {
                                          "properties": {
                                              "granule_id": {"type": "keyword"},
-                                             #"s3_url": {"type": "keyword"},
+                                             # "s3_url": {"type": "keyword"},
                                              "https_url": {"type": "keyword"},
                                              "creation_timestamp": {"type": "date"},
                                              "download_datetime": {"type": "date"},
@@ -45,16 +45,22 @@ class SLCProductCatalog(ElasticsearchUtility):
         if self.logger:
             self.logger.info("Successfully deleted index: {}".format(ES_INDEX))
 
-    def get_all_undownloaded(self, start_dt: datetime, end_dt: datetime):
-        undownloaded = self._query_undownloaded(start_dt, end_dt)
-        return [
-            {
-                #"s3_url": result['_source']['s3_url'],
-                "https_url": result['_source']['https_url']
-            } for result in (undownloaded or [])
-        ]
+    def get_all_undownloaded(self, start_dt: datetime, end_dt: datetime, use_temporal: bool):
+        undownloaded = self._query_undownloaded(start_dt, end_dt, use_temporal)
 
-    def process_url(self, url: str, granule_id: str, job_id: str, query_dt: datetime, temporal_extent_beginning_dt: datetime):
+        return [{  # "s3_url": result['_source']['s3_url'],
+            "https_url": result['_source']['https_url']}
+            for result in (undownloaded or [])]
+
+    def process_url(
+            self,
+            url: str,
+            granule_id: str,
+            job_id: str,
+            query_dt: datetime,
+            temporal_extent_beginning_dt: datetime,
+            revision_date_dt: datetime
+    ):
         filename = Path(url).name
         result = self._query_existence(filename)
         doc = {
@@ -63,13 +69,14 @@ class SLCProductCatalog(ElasticsearchUtility):
             "creation_timestamp": datetime.now(),
             "query_job_id": job_id,
             "query_datetime": query_dt,
-            "temporal_extent_beginning_datetime": temporal_extent_beginning_dt
+            "temporal_extent_beginning_datetime": temporal_extent_beginning_dt,
+            "revision_date": revision_date_dt
         }
 
         if "https://" in url:
             doc["https_url"] = url
-        #elif "s3://" in url:
-            #doc["s3_url"] = url
+        # elif "s3://" in url:
+        # doc["s3_url"] = url
         else:
             raise Exception(f"Unrecognized URL format. {url=}")
 
@@ -92,18 +99,12 @@ class SLCProductCatalog(ElasticsearchUtility):
 
     def mark_product_as_downloaded(self, url, job_id):
         filename = url.split('/')[-1]
-        result = self.update_document(
-            id=filename,
-            body={
-                "doc_as_upsert": True,
-                "doc": {
-                    "downloaded": True,
-                    "download_datetime": datetime.now(),
-                    "download_job_id": job_id,
-                }
-            },
-            index=ES_INDEX
-        )
+        result = self.update_document(id=filename,
+                                      body={"doc_as_upsert": True,
+                                            "doc": {"downloaded": True,
+                                                    "download_datetime": datetime.now(),
+                                                    "download_job_id": job_id, }},
+                                      index=ES_INDEX)
 
         if self.logger:
             self.logger.info(f"Document updated: {result}")
@@ -127,38 +128,15 @@ class SLCProductCatalog(ElasticsearchUtility):
 
         return result
 
-
-    def _query_undownloaded(self, start_dt: datetime, end_dt: datetime, index=ES_INDEX):
+    def _query_undownloaded(self, start_dt: datetime, end_dt: datetime, use_temporal: bool, index=ES_INDEX):
+        range_str = "temporal_extent_beginning_datetime" if use_temporal else "revision_date"
         try:
-            result = self.query(
-                index=index,
-                body={
-                    "sort": [
-                        {
-                            "creation_timestamp": "asc"
-                        }
-                    ],
-                    "query": {
-                        "bool": {
-                            "must": [
-                                {
-                                    "match": {
-                                        "downloaded": False
-                                    }
-                                },
-                                {
-                                    "range": {
-                                        "temporal_extent_beginning_datetime": {
-                                            "gte": start_dt.isoformat(),
-                                            "lt": end_dt.isoformat()
-                                        }
-                                    }
-                                }
-                            ]
-                        }
-                    }
-                }
-            )
+            result = self.query(index=index,
+                                body={"sort": [{"creation_timestamp": "asc"}],
+                                      "query": {"bool": {"must": [{"match": {"downloaded": False}},
+                                                                  {"range": {range_str: {
+                                                                      "gte": start_dt.isoformat(),
+                                                                      "lt": end_dt.isoformat()}}}]}}})
             if self.logger:
                 self.logger.debug(f"Query result: {result}")
 
