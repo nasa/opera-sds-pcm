3a3351aae92e634c629e7c4beea0819bc3a96831
chrisjrd <chris.jrd@gmail.com>
2022-10-03 11:11:16 -0700

    feat(integration-tests): fix missing references. fixed merge issues.
    Refs #208

--- integration/int_test_util.py
+++ integration/int_test_util.py
@@ -58,25 +58,6 @@ def wait_for_l2(_id, index):
 @backoff.on_predicate(
     backoff.constant,
     lambda r: len(r) != 1,
-    max_time=60*10,
-    on_success=success_handler,
-    on_giveup=lambda _: raise_(Exception()),
-    interval=30
-)
-@backoff.on_exception(
-    backoff.constant,
-    elasticsearch.exceptions.NotFoundError,
-    max_time=60*10,
-    giveup=index_not_found,
-    interval=30
-)
-def wait_for_state_config(_id, index):
-    return search_es(index, _id)
-
-
-@backoff.on_predicate(
-    backoff.constant,
-    lambda r: len(r) != 1,
     max_time=60*20,
     on_success=success_handler,
     on_giveup=lambda _: raise_(Exception()),
--- integration/test_integration.py
+++ integration/test_integration.py
@@ -10,8 +10,7 @@ from int_test_util import \
     wait_for_cnm_s_success, \
     wait_for_cnm_r_success, \
     wait_for_l2, \
-    wait_for_l3, \
-    wait_for_state_config
+    wait_for_l3
 from subscriber_util import \
     wait_for_query_job, \
     wait_for_download_jobs, \
@@ -76,152 +75,80 @@ def test_subscriber_s30():
     sleep_for(300)
     wait_for_download_jobs(job_id)
 
-
-def test_l30():
-    logging.info("UPLOADING INPUT FILES")
-
-    download_dir: Path = Path(config["L30_INPUT_DIR"]).expanduser()
-    input_filepaths = [
-        download_dir / "HLS.L30.T22VEQ.2021248T143156.v2.0.B02.tif",
-        download_dir / "HLS.L30.T22VEQ.2021248T143156.v2.0.B03.tif",
-        download_dir / "HLS.L30.T22VEQ.2021248T143156.v2.0.B04.tif",
-        download_dir / "HLS.L30.T22VEQ.2021248T143156.v2.0.B05.tif",
-        download_dir / "HLS.L30.T22VEQ.2021248T143156.v2.0.B06.tif",
-        download_dir / "HLS.L30.T22VEQ.2021248T143156.v2.0.B07.tif",
-        download_dir / "HLS.L30.T22VEQ.2021248T143156.v2.0.Fmask.tif"
-    ]
-    for i, input_filepath in enumerate(input_filepaths):
-        logging.info(f"Uploading file {i+1} of {len(input_filepaths)}")
-        upload_file(filepath=input_filepath)
-
-    logging.info("CHECKING FOR L2 ENTRIES, INDICATING SUCCESSFUL DATA INGEST")
-
-    logging.info("Sleeping for L2 ingest...")
-    sleep_for(150)
-
-    response = wait_for_l2(_id="HLS.L30.T22VEQ.2021248T143156.v2.0.B02", index="grq_v2.0_l2_hls_l30")
-    assert response.hits[0]["id"] == "HLS.L30.T22VEQ.2021248T143156.v2.0.B02"
-
-    response = wait_for_l2(_id="HLS.L30.T22VEQ.2021248T143156.v2.0.B03", index="grq_v2.0_l2_hls_l30")
-    assert response.hits[0]["id"] == "HLS.L30.T22VEQ.2021248T143156.v2.0.B03"
-
-    response = wait_for_l2(_id="HLS.L30.T22VEQ.2021248T143156.v2.0.B04", index="grq_v2.0_l2_hls_l30")
-    assert response.hits[0]["id"] == "HLS.L30.T22VEQ.2021248T143156.v2.0.B04"
-
-    response = wait_for_l2(_id="HLS.L30.T22VEQ.2021248T143156.v2.0.B05", index="grq_v2.0_l2_hls_l30")
-    assert response.hits[0]["id"] == "HLS.L30.T22VEQ.2021248T143156.v2.0.B05"
-
-    response = wait_for_l2(_id="HLS.L30.T22VEQ.2021248T143156.v2.0.B06", index="grq_v2.0_l2_hls_l30")
-    assert response.hits[0]["id"] == "HLS.L30.T22VEQ.2021248T143156.v2.0.B06"
-
-    response = wait_for_l2(_id="HLS.L30.T22VEQ.2021248T143156.v2.0.B07", index="grq_v2.0_l2_hls_l30")
-    assert response.hits[0]["id"] == "HLS.L30.T22VEQ.2021248T143156.v2.0.B07"
-
-    response = wait_for_l2(_id="HLS.L30.T22VEQ.2021248T143156.v2.0.Fmask", index="grq_v2.0_l2_hls_l30")
-    assert response.hits[0]["id"] == "HLS.L30.T22VEQ.2021248T143156.v2.0.Fmask"
-
-    logging.info("Sleeping for state-config execution...")
-    sleep_for(150)
-    response = wait_for_state_config(_id="HLS.L30.T22VEQ.2021248T143156.v2.0_state_config", index="grq_1_l2_hls_l30-state-config")
-    assert response.hits[0]["id"] == "HLS.L30.T22VEQ.2021248T143156.v2.0_state_config"
-
     logging.info("CHECKING FOR L3 ENTRIES, INDICATING SUCCESSFUL PGE EXECUTION")
 
     logging.info("Sleeping for PGE execution...")
     sleep_for(300)
 
-    response = wait_for_l3(_id="OPERA_L3_DSWx_HLS_T22VEQ_20210905T143156Z_20210905T143156Z_L8_30_v2.0", index="grq_v2.0_l3_dswx_hls")
-    assert response.hits[0]["id"] == "OPERA_L3_DSWx_HLS_T22VEQ_20210905T143156Z_20210905T143156Z_L8_30_v2.0"
+    response = wait_for_l3(_id="OPERA_L3_DSWx_HLS_T54PVQ_20220101T005855Z_20220101T005855Z_L8_30_v2.0", index="grq_v2.0_l3_dswx_hls")
+    assert response.hits[0]["id"] == "OPERA_L3_DSWx_HLS_T54PVQ_20220101T005855Z_20220101T005855Z_L8_30_v2.0"
 
     logging.info("CHECKING FOR CNM-S SUCCESS")
 
     logging.info("Sleeping for CNM-S execution...")
     sleep_for(150)
 
-    response = wait_for_cnm_s_success(_id="OPERA_L3_DSWx_HLS_T22VEQ_20210905T143156Z_20210905T143156Z_L8_30_v2.0", index="grq_v2.0_l3_dswx_hls")
+    response = wait_for_cnm_s_success(_id="OPERA_L3_DSWx_HLS_T54PVQ_20220101T005855Z_20220101T005855Z_L8_30_v2.0", index="grq_v2.0_l3_dswx_hls")
     assert_cnm_s_success(response)
 
     logging.info("TRIGGER AND CHECK FOR CNM-R SUCCESS")
-    mock_cnm_r_success_sns(id="OPERA_L3_DSWx_HLS_T22VEQ_20210905T143156Z_20210905T143156Z_L8_30_v2.0")
+    mock_cnm_r_success_sns(id="OPERA_L3_DSWx_HLS_T54PVQ_20220101T005855Z_20220101T005855Z_L8_30_v2.0")
 
     logging.info("Sleeping for CNM-R execution...")
     sleep_for(150)
 
-    response = wait_for_cnm_r_success(_id="OPERA_L3_DSWx_HLS_T22VEQ_20210905T143156Z_20210905T143156Z_L8_30_v2.0", index="grq_v2.0_l3_dswx_hls")
+    response = wait_for_cnm_r_success(_id="OPERA_L3_DSWx_HLS_T54PVQ_20220101T005855Z_20220101T005855Z_L8_30_v2.0", index="grq_v2.0_l3_dswx_hls")
     assert_cnm_r_success(response)
 
 
-def test_s30():
-    logging.info("UPLOADING INPUT FILES")
+def test_subscriber_s30():
+    logging.info("TRIGGERING DATA SUBSCRIBE")
 
-    download_dir: Path = Path(config["S30_INPUT_DIR"]).expanduser()
-    input_filepaths = [
-        download_dir / "HLS.S30.T15SXR.2021250T163901.v2.0.B02.tif",
-        download_dir / "HLS.S30.T15SXR.2021250T163901.v2.0.B03.tif",
-        download_dir / "HLS.S30.T15SXR.2021250T163901.v2.0.B04.tif",
-        download_dir / "HLS.S30.T15SXR.2021250T163901.v2.0.B11.tif",
-        download_dir / "HLS.S30.T15SXR.2021250T163901.v2.0.B12.tif",
-        download_dir / "HLS.S30.T15SXR.2021250T163901.v2.0.B8A.tif",
-        download_dir / "HLS.S30.T15SXR.2021250T163901.v2.0.Fmask.tif"
-    ]
-    for i, input_filepath in enumerate(input_filepaths):
-        logging.info(f"Uploading file {i+1} of {len(input_filepaths)}")
-        upload_file(filepath=input_filepath)
+    update_env_vars_s30_subscriber_query_lambda()
+    sleep_for(30)
 
-    logging.info("CHECKING FOR L2 ENTRIES, INDICATING SUCCESSFUL DATA INGEST")
+    response = invoke_s30_subscriber_query_lambda()
 
-    logging.info("Sleeping for L2 ingest...")
+    reset_env_vars_s30_subscriber_query_lambda()
+    sleep_for(30)
+
+    assert response["StatusCode"] == 200
+
+    job_id = response["Payload"].read().decode().strip("\"")
+    logging.info(f"{job_id=}")
+
+    logging.info("Sleeping for query job execution...")
     sleep_for(150)
 
-    response = wait_for_l2(_id="HLS.S30.T15SXR.2021250T163901.v2.0.B02", index="grq_v2.0_l2_hls_s30")
-    assert response.hits[0]["id"] == "HLS.S30.T15SXR.2021250T163901.v2.0.B02"
+    wait_for_query_job(job_id)
 
-    response = wait_for_l2(_id="HLS.S30.T15SXR.2021250T163901.v2.0.B03", index="grq_v2.0_l2_hls_s30")
-    assert response.hits[0]["id"] == "HLS.S30.T15SXR.2021250T163901.v2.0.B03"
-
-    response = wait_for_l2(_id="HLS.S30.T15SXR.2021250T163901.v2.0.B04", index="grq_v2.0_l2_hls_s30")
-    assert response.hits[0]["id"] == "HLS.S30.T15SXR.2021250T163901.v2.0.B04"
-
-    response = wait_for_l2(_id="HLS.S30.T15SXR.2021250T163901.v2.0.B11", index="grq_v2.0_l2_hls_s30")
-    assert response.hits[0]["id"] == "HLS.S30.T15SXR.2021250T163901.v2.0.B11"
-
-    response = wait_for_l2(_id="HLS.S30.T15SXR.2021250T163901.v2.0.B12", index="grq_v2.0_l2_hls_s30")
-    assert response.hits[0]["id"] == "HLS.S30.T15SXR.2021250T163901.v2.0.B12"
-
-    response = wait_for_l2(_id="HLS.S30.T15SXR.2021250T163901.v2.0.B8A", index="grq_v2.0_l2_hls_s30")
-    assert response.hits[0]["id"] == "HLS.S30.T15SXR.2021250T163901.v2.0.B8A"
-
-    response = wait_for_l2(_id="HLS.S30.T15SXR.2021250T163901.v2.0.Fmask", index="grq_v2.0_l2_hls_s30")
-    assert response.hits[0]["id"] == "HLS.S30.T15SXR.2021250T163901.v2.0.Fmask"
-
-    logging.info("Sleeping for state-config execution...")
+    logging.info("Sleeping for download job execution...")
     sleep_for(150)
-    response = wait_for_state_config(_id="HLS.S30.T15SXR.2021250T163901.v2.0_state_config", index="grq_1_l2_hls_s30-state-config")
-    assert response.hits[0]["id"] == "HLS.S30.T15SXR.2021250T163901.v2.0_state_config"
+    wait_for_download_jobs(job_id)
 
     logging.info("CHECKING FOR L3 ENTRIES, INDICATING SUCCESSFUL PGE EXECUTION")
 
     logging.info("Sleeping for PGE execution...")
     sleep_for(150)
 
-    response = wait_for_l3(_id="OPERA_L3_DSWx_HLS_T15SXR_20210907T163901Z_20210907T163901Z_S2A_30_v2.0", index="grq_v2.0_l3_dswx_hls")
-    assert response.hits[0]["id"] == "OPERA_L3_DSWx_HLS_T15SXR_20210907T163901Z_20210907T163901Z_S2A_30_v2.0"
+    response = wait_for_l3(_id="OPERA_L3_DSWx_HLS_T53HQV_20220101T003711Z_20220101T003711Z_S2A_30_v2.0", index="grq_v2.0_l3_dswx_hls")
+    assert response.hits[0]["id"] == "OPERA_L3_DSWx_HLS_T53HQV_20220101T003711Z_20220101T003711Z_S2A_30_v2.0"
 
     logging.info("CHECKING FOR CNM-S SUCCESS")
 
     logging.info("Sleeping for CNM-S execution...")
     sleep_for(150)
 
-    response = wait_for_cnm_s_success(_id="OPERA_L3_DSWx_HLS_T15SXR_20210907T163901Z_20210907T163901Z_S2A_30_v2.0", index="grq_v2.0_l3_dswx_hls")
+    response = wait_for_cnm_s_success(_id="OPERA_L3_DSWx_HLS_T53HQV_20220101T003711Z_20220101T003711Z_S2A_30_v2.0", index="grq_v2.0_l3_dswx_hls")
     assert_cnm_s_success(response)
 
     logging.info("TRIGGER AND CHECK FOR CNM-R SUCCESS")
-    mock_cnm_r_success_sns(id="OPERA_L3_DSWx_HLS_T15SXR_20210907T163901Z_20210907T163901Z_S2A_30_v2.0")
+    mock_cnm_r_success_sns(id="OPERA_L3_DSWx_HLS_T53HQV_20220101T003711Z_20220101T003711Z_S2A_30_v2.0")
 
     logging.info("Sleeping for CNM-R execution...")
     sleep_for(150)
 
-    response = wait_for_cnm_r_success(_id="OPERA_L3_DSWx_HLS_T15SXR_20210907T163901Z_20210907T163901Z_S2A_30_v2.0", index="grq_v2.0_l3_dswx_hls")
+    response = wait_for_cnm_r_success(_id="OPERA_L3_DSWx_HLS_T53HQV_20220101T003711Z_20220101T003711Z_S2A_30_v2.0", index="grq_v2.0_l3_dswx_hls")
     assert_cnm_r_success(response)
 
 
