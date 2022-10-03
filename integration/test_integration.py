d195d9618631ba8c180c2a17b1c01c053b5b0f5b
chrisjrd <chris.jrd@gmail.com>
2022-10-03 11:13:36 -0700

    feat(integration-tests): fixed merge issues.
    Refs #208

--- integration/test_integration.py
+++ integration/test_integration.py
@@ -49,32 +49,6 @@ def test_subscriber_l30():
     sleep_for(300)
     wait_for_download_jobs(job_id)
 
-
-def test_subscriber_s30():
-    logging.info("TRIGGERING DATA SUBSCRIBE")
-
-    update_env_vars_s30_subscriber_query_lambda()
-    sleep_for(30)
-
-    response = invoke_s30_subscriber_query_lambda()
-
-    reset_env_vars_s30_subscriber_query_lambda()
-    sleep_for(30)
-
-    assert response["StatusCode"] == 200
-
-    job_id = response["Payload"].read().decode().strip("\"")
-    logging.info(f"{job_id=}")
-
-    logging.info("Sleeping for query job execution...")
-    sleep_for(300)
-
-    wait_for_query_job(job_id)
-
-    logging.info("Sleeping for download job execution...")
-    sleep_for(300)
-    wait_for_download_jobs(job_id)
-
     logging.info("CHECKING FOR L3 ENTRIES, INDICATING SUCCESSFUL PGE EXECUTION")
 
     logging.info("Sleeping for PGE execution...")
