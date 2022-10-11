8081944e61d0438f5d97d57b08ac8b30a02d4a25
chrisjrd <chris.jrd@gmail.com>
2022-10-11 12:56:51 -0700

    feat(metadata): fix s3 object URL region parsing
    Refs #243

--- util/datasets_json_util.py
+++ util/datasets_json_util.py
@@ -34,7 +34,8 @@ def find_region(datasets_json, dataset_type):
     """Extracts the region from the publish location. See find_publish_location_s3
     """
     publish_location = find_publish_location_s3(datasets_json, dataset_type)
-    return PurePath(publish_location.parts[1].split()[0]).with_suffix("").with_suffix("")
+    region_fragment = PurePath(publish_location.parts[1].split()[0]).with_suffix("").with_suffix("")  # e.g. "s3-us-west-2"
+    return str(region_fragment).removeprefix("s3-")
 
 
 def find_s3_url(datasets_json, dataset_type):
