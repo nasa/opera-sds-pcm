4b82fb946a85d5636a1da29d4bcf3946f9b30992
hyunlee <Hyun.H.Lee@jpl.nasa.gov>
2022-10-23 23:08:23 -0700

    Update datasets_json_util.py

--- util/datasets_json_util.py
+++ util/datasets_json_util.py
@@ -1,7 +1,7 @@
 from pathlib import PurePath
 
 
-def find_s3_bucket(datasets_json, dataset_type):
+def find_publish_location_s3(datasets_json, dataset_type):
     """Example location: "s3://{{ DATASET_S3_ENDPOINT }}:80/{{ DATASET_BUCKET }}/products/{id}"
     """
     publish_location = None
@@ -13,7 +13,29 @@ def find_s3_bucket(datasets_json, dataset_type):
 
     if publish_location is None:
         raise Exception("s3 bucket not found")
+    return PurePath(publish_location)
+
+
+def find_dataset_s3_endpoint(datasets_json, dataset_type):
+    """Example location: "s3://{{ DATASET_S3_ENDPOINT }}:80/{{ DATASET_BUCKET }}/products/{id}"
+    """
+    publish_location = find_publish_location_s3(datasets_json, dataset_type)
+    return PurePath(publish_location).parts[1]
+
+
+def find_s3_bucket(datasets_json, dataset_type):
+    """Example location: "s3://{{ DATASET_S3_ENDPOINT }}:80/{{ DATASET_BUCKET }}/products/{id}"
+    """
+    publish_location = find_publish_location_s3(datasets_json, dataset_type)
     return PurePath(publish_location).parts[2]
+
+
+def find_region(datasets_json, dataset_type):
+    """Extracts the region from the publish location. See find_publish_location_s3
+    """
+    publish_location = find_publish_location_s3(datasets_json, dataset_type)
+    region_fragment = PurePath(publish_location.parts[1].split()[0]).with_suffix("").with_suffix("")  # e.g. "s3-us-west-2"
+    return str(region_fragment).removeprefix("s3-")
 
 
 def find_s3_url(datasets_json, dataset_type):
