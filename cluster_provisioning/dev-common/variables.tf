a5570ef0f6665d9be01599327c48abba17a1636b
Scott Collins <collinss@jpl.nasa.gov>
2022-11-01 14:17:47 -0700

    Updated pge_releases to point at newest R2 PGE releases, including RTC-S1, for all Terraform venues

--- cluster_provisioning/dev-common/variables.tf
+++ cluster_provisioning/dev-common/variables.tf
@@ -353,7 +353,8 @@ variable "pge_releases" {
   type = map(string)
   default = {
     "dswx_hls" = "1.0.0-rc.5.0"
-    "cslc_s1" = "2.0.0-er.2.0"
+    "cslc_s1" = "2.0.0-er.3.0"
+    "rtc_s1" = "2.0.0-er.3.0"
   }
 }
 
