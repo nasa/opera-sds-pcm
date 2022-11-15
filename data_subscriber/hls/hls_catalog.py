f35b9915e84268868c0147e2abbc9105bd76a29d
chrisjrd <chris.jrd@gmail.com>
2022-11-15 14:00:11 -0800

    feat(subscriber): add required upsert property to ES call body
    Refs #288

--- data_subscriber/hls/hls_catalog.py
+++ data_subscriber/hls/hls_catalog.py
@@ -78,7 +78,7 @@ class HLSProductCatalog(ElasticsearchUtility):
         else:
             raise Exception(f"Unrecognized URL format. {url=}")
 
-        self.update_document(index=ES_INDEX, body={"doc": doc}, id=filename)
+        self.update_document(index=ES_INDEX, body={"doc_as_upsert": True, "doc": doc}, id=filename)
         return True
 
     def product_is_downloaded(self, url):
