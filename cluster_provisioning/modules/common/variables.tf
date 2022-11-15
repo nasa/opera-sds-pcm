5b2d7110f1a4da356b71c447348ebb63de02f8ed
hyunlee <Hyun.H.Lee@jpl.nasa.gov>
2022-11-15 08:50:32 -0800

    Change product_delivery_branch to 'develop'

--- cluster_provisioning/modules/common/variables.tf
+++ cluster_provisioning/modules/common/variables.tf
@@ -133,11 +133,11 @@ variable "amis" {
   type = map(string)
   default = {
     # HySDS v4.1.0-beta.4 
-	mozart    = "ami-0a4c8f9c7f5a2daec" # mozart v4.18 - 221107
+    mozart    = "ami-0a4c8f9c7f5a2daec" # mozart v4.18 - 221107
     metrics   = "ami-0c61e7c8b1bfd14a3" # metrics v4.13 - 221107
     grq       = "ami-0f52442c2bd506303" # grq v4.14 - 221107 
     factotum  = "ami-03fdbdb8c7caa736e" # factotum v4.14 - 221107
-	autoscale = "ami-003e368c872ea1099" # verdi v4.15 - 221031
+    autoscale = "ami-003e368c872ea1099" # verdi v4.15 - 221031
   }
 }
 
