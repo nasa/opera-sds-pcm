1860ff9d1cbef5dc8ef6c565697e76158c92df9b
hyunlee <Hyun.H.Lee@jpl.nasa.gov>
2023-01-30 16:13:24 -0800

    Update AMIs with ES 7.10

--- cluster_provisioning/modules/common/variables.tf
+++ cluster_provisioning/modules/common/variables.tf
@@ -133,11 +133,19 @@ variable "amis" {
   type = map(string)
   default = {
     # HySDS v4.1.0-beta.4
-    mozart    = "ami-0a4c8f9c7f5a2daec" # mozart v4.18 - 221107
-    metrics   = "ami-0c61e7c8b1bfd14a3" # metrics v4.13 - 221107
-    grq       = "ami-0f52442c2bd506303" # grq v4.14 - 221107
-    factotum  = "ami-03fdbdb8c7caa736e" # factotum v4.14 - 221107
-    autoscale = "ami-003e368c872ea1099" # verdi v4.15 - 221031
+    #mozart    = "ami-0a4c8f9c7f5a2daec" # mozart v4.18 - 221107
+    #metrics   = "ami-0c61e7c8b1bfd14a3" # metrics v4.13 - 221107
+    #grq       = "ami-0f52442c2bd506303" # grq v4.14 - 221107
+    #factotum  = "ami-03fdbdb8c7caa736e" # factotum v4.14 - 221107
+    #autoscale = "ami-003e368c872ea1099" # verdi v4.15 - 221031
+
+
+    # HySDS v4.1.0-beta.4 with ES 7.10 - Jan 20, 2023
+	mozart    = "ami-07227c9552d105f5d"
+    metrics   = "ami-076eed77dcca47ddd"
+    grq       = "ami-077e344b9281d76af"
+    factotum  = "ami-024bb9a07340e8438"
+    autoscale = "ami-0824aba5572a8695f"
   }
 }
 
