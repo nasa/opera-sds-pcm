e2a35b837e1011a2623d4657c396928b8361b9a7
hyunlee <Hyun.H.Lee@jpl.nasa.gov>
2023-02-14 08:59:05 -0800

    Add root_dev_zie for grq and metrics variables in dev-e2e terraform scripts

--- cluster_provisioning/dev-restore-snapshot/variables.tf
+++ cluster_provisioning/dev-restore-snapshot/variables.tf
@@ -177,6 +177,7 @@ variable "metrics" {
   default = {
     name          = "metrics"
     instance_type = "r5.xlarge"
+    root_dev_size = 50
     private_ip    = ""
     public_ip     = ""
   }
@@ -188,6 +189,7 @@ variable "grq" {
   default = {
     name          = "grq"
     instance_type = "r5.xlarge"
+    root_dev_size = 50
     private_ip    = ""
     public_ip     = ""
   }
