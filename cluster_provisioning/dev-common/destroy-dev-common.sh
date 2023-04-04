4a4ecb93fe566a4f35f4ee0d63e1296ce8a07887
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2023-04-03 17:10:35 -0700

    Trivial change for some of the terraform helper scripts

--- cluster_provisioning/dev-common/destroy-dev-common.sh
+++ cluster_provisioning/dev-common/destroy-dev-common.sh
@@ -1,6 +1,4 @@
-FIX THIS
-
-aws-login -pub
-~/terraform destroy -var-file=dev-common.tfvars
-aws --profile saml-pub logs describe-log-groups --log-group-name-prefix /aws/lambda/opera-dev-fwd | grep logGroupName | cut -d '"' -f4 | xargs -i -t aws --profile saml-pub logs delete-log-group --log-group-name {}
+aws-login -pub &&
+~/terraform destroy -var-file=dev-common.tfvars &&
+aws --profile saml-pub logs describe-log-groups --log-group-name-prefix /aws/lambda/opera-dev-fwd | grep logGroupName | cut -d '"' -f4 | xargs -i -t aws --profile saml-pub logs delete-log-group --log-group-name {} &&
 aws --profile saml-pub ec2 describe-launch-templates | grep LaunchTemplateName | grep dev-fwd | cut -d '"' -f4 | xargs -i -t aws --profile saml-pub ec2 delete-launch-template --launch-template-name {}
