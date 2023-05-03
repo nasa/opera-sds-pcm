41dc8023d6da58eafc08fdbba03f68285227ff2a
philipjyoon <104090313+philipjyoon@users.noreply.github.com>
2023-05-03 15:44:10 -0700

    Update int_pst_ops_devcommon_override.tf
    Updating version numbers

--- cluster_provisioning/int_pst_ops_devcommon_override.tf
+++ cluster_provisioning/int_pst_ops_devcommon_override.tf
@@ -6,25 +6,25 @@ variable "hysds_release" {
 }
 
 variable "lambda_package_release" {
-  default = "2.0.0-rc.4.0"
+  default = "2.0.0-rc.6.0"
 }
 
 variable "pcm_commons_branch" {
-  default = "2.0.0-rc.4.0"
+  default = "2.0.0-rc.6.0"
 }
 
 variable "pcm_branch" {
-  default = "2.0.0-rc.4.0"
+  default = "2.0.0-rc.6.0"
 }
 
 variable "product_delivery_branch" {
-  default = "2.0.0-rc.4.0"
+  default = "2.0.0-rc.6.0"
 }
 
 variable "bach_api_branch" {
-  default = "2.0.0-rc.4.0"
+  default = "2.0.0-rc.6.0"
 }
 
 variable "bach_ui_branch" {
-  default = "2.0.0-rc.4.0"
+  default = "2.0.0-rc.6.0"
 }
