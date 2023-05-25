d99b7553a80eca57b8802f6b1c5a64d9f1abccb7
hyunlee <Hyun.H.Lee@jpl.nasa.gov>
2023-05-25 13:44:55 -0700

    Add template files for HySDS v5.0.0-beta.6 & update AMIs

---
+++ conf/sds/files/test/purge_rs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export GRQ_PVT_IP=$(grep ^GRQ_PVT_IP ~/.sds/config | cut -d: -f2 | xargs)
+export DATASET_BUCKET=$(grep ^DATASET_BUCKET ~/.sds/config | cut -d: -f2 | xargs)
+
+# delete indices
+for i in `curl "${GRQ_PVT_IP}:9200/_cat/indices/grq_*" | awk '{print $3}'`; do
+  echo -n "deleting $i..."
+  curl -XDELETE "${GRQ_PVT_IP}:9200/${i}"
+  echo "done."
+done
+
+# delete bucket dirs
+aws s3 rm --recursive s3://${DATASET_BUCKET}/products
+aws s3 rm --recursive s3://${DATASET_BUCKET}/inputs
+
