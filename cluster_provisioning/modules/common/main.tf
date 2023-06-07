5643138ff0ef6727f01e626d7cf75d5ebfcabd20
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2023-06-07 15:58:49 -0700

    Issue #514: Moving template code out of main.tf into template file

--- cluster_provisioning/modules/common/main.tf
+++ cluster_provisioning/modules/common/main.tf
@@ -88,9 +88,6 @@ resource "null_resource" "download_lambdas" {
     command = "curl -H \"X-JFrog-Art-Api:${var.artifactory_fn_api_key}\" -O ${local.lambda_repo}/${var.lambda_package_release}/${var.lambda_data-subscriber-query_handler_package_name}-${var.lambda_package_release}.zip"
   }
   provisioner "local-exec" {
-    command = "curl -H \"X-JFrog-Art-Api:${var.artifactory_fn_api_key}\" -O ${local.lambda_repo}/${var.lambda_package_release}/${var.lambda_data-subscriber-download-slc-ionosphere_handler_package_name}-${var.lambda_package_release}.zip"
-  }
-  provisioner "local-exec" {
     command = "curl -H \"X-JFrog-Art-Api:${var.artifactory_fn_api_key}\" -O ${local.lambda_repo}/${var.lambda_package_release}/${var.lambda_batch-query_handler_package_name}-${var.lambda_package_release}.zip"
   }
 }
@@ -1558,5 +1555,150 @@ resource "null_resource" "rs_fwd_add_lifecycle_rule" {
 
 }
 
+############################
+# Autoscaling Group related
+############################
+
+data "aws_subnet_ids" "private_asg_vpc" {
+  vpc_id = var.private_asg_vpc
+}
+data "aws_subnet_ids" "public_asg_vpc" {
+  vpc_id = var.public_asg_vpc
+}
+
+data "template_file" "launch_template_user_data" {
+  for_each = var.queues
+  template = "${file("${path.module}/template_files/launch_template_user_data.tpl")}"
+}
+
+resource "aws_launch_template" "launch_template" {
+  depends_on = [data.template_file.launch_template_user_data]
+
+  for_each               = var.queues
+  name                   = "${var.project}-${var.venue}-${local.counter}-${each.key}-launch-template"
+  image_id               = var.amis["autoscale"]
+  key_name               = local.key_name
+  user_data              = base64encode(data.template_file.launch_template_user_data[each.key].rendered)
+  vpc_security_group_ids = [lookup(each.value, "use_private_vpc", true) ? var.private_verdi_security_group_id : var.public_verdi_security_group_id]
+
+  tags = { Bravo = "pcm" }
+  block_device_mappings {
+    device_name = "/dev/sda1"
+    ebs {
+      volume_size           = lookup(each.value, "root_dev_size")
+      delete_on_termination = true
+    }
+  }
+
+  block_device_mappings {
+    device_name = "/dev/sdf"
+    ebs {
+      volume_size = lookup(each.value, "data_dev_size")
+      snapshot_id = data.aws_ebs_snapshot.docker_verdi_registry.id
+    }
+  }
+
+  iam_instance_profile {
+    name = var.asg_use_role ? var.asg_role : ""
+  }
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = {
+      Bravo = "pcm"
+    }
+  }
+  #This is very important, as it tells terraform to not mess with tags
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "aws_autoscaling_group" "autoscaling_group" {
+  for_each                  = var.queues
+  name                      = "${var.project}-${var.venue}-${local.counter}-${each.key}"
+  depends_on                = [aws_launch_template.launch_template]
+  max_size                  = lookup(each.value, "max_size")
+  min_size                  = lookup(each.value, "min_size", 0)
+  default_cooldown          = 60
+  desired_capacity          = lookup(each.value, "min_size", 0)
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+  protect_from_scale_in     = false
+  vpc_zone_identifier       = lookup(each.value, "use_private_vpc", true) ? data.aws_subnet_ids.private_asg_vpc.ids : data.aws_subnet_ids.public_asg_vpc.ids
+  metrics_granularity       = "1Minute"
+  enabled_metrics = [
+    "GroupMinSize",
+    "GroupMaxSize",
+    "GroupDesiredCapacity",
+    "GroupInServiceInstances",
+    "GroupPendingInstances",
+    "GroupStandbyInstances",
+    "GroupTerminatingInstances",
+    "GroupTotalInstances"
+  ]
+  tags = [
+    {
+      key                 = "Name"
+      value               = "${var.project}-${var.venue}-${local.counter}-${each.key}"
+      propagate_at_launch = true
+    },
+#    {
+#      key                 = "Venue"
+#      value               = "${var.project}-${var.venue}-${local.counter}"
+#      propagate_at_launch = true
+#    },
+    {
+      key                 = "Queue"
+      value               = each.key
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Bravo"
+      value               = "pcm"
+      propagate_at_launch = true
+    },
+  ]
+  mixed_instances_policy {
+    instances_distribution {
+      spot_allocation_strategy                 = "lowest-price"
+      spot_instance_pools                      = 3
+      on_demand_base_capacity                  = 0
+      on_demand_percentage_above_base_capacity = 0
+    }
+
+    launch_template {
+      launch_template_specification {
+        launch_template_name = "${var.project}-${var.venue}-${local.counter}-${each.key}-launch-template"
+        version              = "$Latest"
+      }
+
+      dynamic "override" {
+        for_each = toset(lookup(each.value, "instance_type"))
+        content {
+          instance_type = override.value
+        }
+      }
+    }
+  }
+  #This is very important, as it tells terraform to not mess with tags
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "aws_autoscaling_policy" "autoscaling_policy" {
+  for_each               = var.queues
+  name                   = "${var.project}-${var.venue}-${local.counter}-${each.key}-target-tracking"
+  policy_type            = "TargetTrackingScaling"
+  autoscaling_group_name = "${var.project}-${var.venue}-${local.counter}-${each.key}"
+  depends_on             = [aws_autoscaling_group.autoscaling_group]
+  target_tracking_configuration {
+    customized_metric_specification {
+
+      metric_dimension {
+        name  = "AutoScalingGroupName"
+        value = "${var.project}-${var.venue}-${local.counter}-${each.key}"
+      }
 
 
