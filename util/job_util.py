5ebb711e558612404bc0215b06ea5ed2cfe148f8
chrisjrd <chris.jrd@gmail.com>
2023-06-12 16:00:59 -0700

    feature(slc): lower log level for job context.
    info no longer useful to see taking up log space.
    
    Refs #478

--- util/job_util.py
+++ util/job_util.py
@@ -12,13 +12,13 @@ def is_running_outside_verdi_worker_context():
 
 def supply_job_id():
     if is_running_outside_verdi_worker_context():
-        logger.info("Running outside of job context. Generating random job ID")
+        logger.debug("Running outside of job context. Generating random job ID")
         job_id = uuid.uuid4()
     else:
         with open("_job.json", "r+") as job:
-            logger.info("job_path: {}".format(job))
+            logger.debug("job_path: {}".format(job))
             local_job_json = json.load(job)
-            logger.info(f"{local_job_json=!s}")
+            logger.debug(f"{local_job_json=!s}")
         job_id = local_job_json["job_info"]["job_payload"]["payload_task_id"]
 
     logger.info(f"{job_id=}")
