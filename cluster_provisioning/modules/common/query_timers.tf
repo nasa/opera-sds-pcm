ade87ec99a7162dcab8fc5c0360bba572763d234
hyunlee <Hyun.H.Lee@jpl.nasa.gov>
2023-06-21 17:01:31 -0700

    Add ionosphere query timer into query_timers.tf

--- cluster_provisioning/modules/common/query_timers.tf
+++ cluster_provisioning/modules/common/query_timers.tf
@@ -299,6 +299,59 @@ resource "aws_lambda_permission" "slcs1a_query_timer" {
   function_name = aws_lambda_function.slcs1a_query_timer.function_name
 }
 
+resource "aws_lambda_function" "slc_ionosphere_download_timer" {
+  depends_on = [null_resource.download_lambdas]
+  filename = "${var.lambda_data-subscriber-download-slc-ionosphere_handler_package_name}-${var.lambda_package_release}.zip"
+  description = "Lambda function to submit a job that will download ionosphere correction files for SLC products."
+  function_name = "${var.project}-${var.venue}-${local.counter}-slc-ionosphere-download-timer"
+  handler = "lambda_function.lambda_handler"
+  role = var.lambda_role_arn
+  runtime = "python3.8"
+  vpc_config {
+    security_group_ids = [var.cluster_security_group_id]
+    subnet_ids = data.aws_subnet_ids.lambda_vpc.ids
+  }
+  timeout = 30
+  environment {
+    variables = {
+      "MOZART_URL": "https://${aws_instance.mozart.private_ip}/mozart",
+      "JOB_QUEUE": "opera-job_worker-slc_data_download_ionosphere",
+      "JOB_TYPE": local.slc_ionosphere_download_job_type,
+      "JOB_RELEASE": var.pcm_branch
+      "QUERY_START_DATETIME_OFFSET_HOURS": "372"  # 24h x 14d == 336h. Plus QUERY_END offset
+      "QUERY_END_DATETIME_OFFSET_HOURS": "36"  # 36h is the expected maximum latency for ionosphere availability for a *current* SLC product
+    }
+  }
+}
+resource "aws_cloudwatch_log_group" "slc_ionosphere_download_timer" {
+  depends_on = [aws_lambda_function.slc_ionosphere_download_timer]
+  name = "/aws/lambda/${aws_lambda_function.slc_ionosphere_download_timer.function_name}"
+  retention_in_days = var.lambda_log_retention_in_days
+}
+
+resource "aws_cloudwatch_event_rule" "slc_ionosphere_download_timer" {
+  name = "${aws_lambda_function.slc_ionosphere_download_timer.function_name}-Trigger"
+  description = "Cloudwatch event to trigger the Data Subscriber Ionosphere Download Timer Lambda"
+  schedule_expression = var.slc_ionosphere_download_timer_trigger_frequency
+  is_enabled = local.enable_download_timer
+  depends_on = [null_resource.install_pcm_and_pges]
+}
+
+resource "aws_cloudwatch_event_target" "slc_ionosphere_download_timer" {
+  rule = aws_cloudwatch_event_rule.slc_ionosphere_download_timer.name
+  target_id = "Lambda"
+  arn = aws_lambda_function.slc_ionosphere_download_timer.arn
+}
+
+resource "aws_lambda_permission" "slc_ionosphere_download_timer" {
+  statement_id = aws_cloudwatch_event_rule.slc_ionosphere_download_timer.name
+  action = "lambda:InvokeFunction"
+  principal = "events.amazonaws.com"
+  source_arn = aws_cloudwatch_event_rule.slc_ionosphere_download_timer.arn
+  function_name = aws_lambda_function.slc_ionosphere_download_timer.function_name
+}
+
+
 # Batch Query Lambda and Timer ---->
 
 resource "aws_lambda_function" "batch_query_timer" {
