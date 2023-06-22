fcdde75424f0a8377d74a2eaa48347fdb0fd5c81
hyunlee <Hyun.H.Lee@jpl.nasa.gov>
2023-06-21 17:52:01 -0700

    - Update CollectionName and productVersion for CSLC & RTC - Update branch version to 2.0.0-rc.8.0 and hysds release to v5.0.0-beta.6

--- cluster_provisioning/dev-int/override.tf
+++ cluster_provisioning/dev-int/override.tf
@@ -141,12 +141,12 @@ variable "artifactory_repo" {
 variable "amis" {
   type = map(string)
   default = {
-    # HySDS v4.1.0-beta.4 with ES 7.10 - May 2, 2023
-    mozart    = "ami-0fd88bd3fb153cf0b" # mozart v4.19 - 230502
-    metrics   = "ami-0c9e158e347d31fd2" # metrics v4.14 - 230502
-    grq       = "ami-04ad27835f0f0415f" # grq v4.15.1 - 230502
-    factotum  = "ami-06e5d7e4ded54c393" # factotum v4.15 - 230502
-    autoscale = "ami-084cd8a23f570e7c9" # verdi v4.16 patchupdate - 230502
+    # HySDS v5.0.0-beta.6 - May 25, 2023 - R2 RC8.0
+    mozart    = "ami-02cf73926477eae15" # mozart v4.20
+    metrics   = "ami-0e1371110b9744042" # metrics v4.15
+    grq       = "ami-01de15c2a056ba449" # grq v4.16
+    factotum  = "ami-0b988a1203b7e5a58" # factotum v4.16
+    autoscale = "ami-082d3efc94d50659f" # verdi v4.16 patchupdate - 20230525
   }
 }
 
@@ -179,31 +179,31 @@ variable "pge_releases" {
 }
 
 variable "hysds_release" {
-  default = "v4.1.0-beta.4"
+  default = "v5.0.0-beta.6"
 }
 
 variable "lambda_package_release" {
-  default = "2.0.0-rc.7.0"
+  default = "2.0.0-rc.8.0"
 }
 
 variable "pcm_commons_branch" {
-  default = "2.0.0-rc.7.0"
+  default = "2.0.0-rc.8.0"
 }
 
 variable "pcm_branch" {
-  default = "2.0.0-rc.7.0"
+  default = "2.0.0-rc.8.0"
 }
 
 variable "product_delivery_branch" {
-  default = "2.0.0-rc.7.0"
+  default = "2.0.0-rc.8.0"
 }
 
 variable "bach_api_branch" {
-  default = "2.0.0-rc.7.0"
+  default = "2.0.0-rc.8.0"
 }
 
 variable "bach_ui_branch" {
-  default = "2.0.0-rc.7.0"
+  default = "2.0.0-rc.8.0"
 }
 
 ###### Roles ########
--- cluster_provisioning/int_pst_ops_devcommon_override.tf
+++ cluster_provisioning/int_pst_ops_devcommon_override.tf
@@ -2,29 +2,29 @@
 # testing, PST processing, and operations.ß
 
 variable "hysds_release" {
-  default = "v4.1.0-beta.4"
+  default = "v5.0.0-beta.6"
 }
 
 variable "lambda_package_release" {
-  default = "2.0.0-rc.7.0"
+  default = "2.0.0-rc.8.0"
 }
 
 variable "pcm_commons_branch" {
-  default = "2.0.0-rc.7.0"
+  default = "2.0.0-rc.8.0"
 }
 
 variable "pcm_branch" {
-  default = "2.0.0-rc.7.0"
+  default = "2.0.0-rc.8.0"
 }
 
 variable "product_delivery_branch" {
-  default = "2.0.0-rc.7.0"
+  default = "2.0.0-rc.8.0"
 }
 
 variable "bach_api_branch" {
-  default = "2.0.0-rc.7.0"
+  default = "2.0.0-rc.8.0"
 }
 
 variable "bach_ui_branch" {
-  default = "2.0.0-rc.7.0"
+  default = "2.0.0-rc.8.0"
 }
