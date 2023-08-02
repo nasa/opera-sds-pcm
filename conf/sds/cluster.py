2da9a583307bc034eaff29ab16081ab541ff8449
chrisjrd <chris.jrd@gmail.com>
2023-08-02 13:02:50 -0700

    feat(grq): fix syntax problem
    Refs #552

--- conf/sds/cluster.py
+++ conf/sds/cluster.py
@@ -257,7 +257,7 @@ def create_ilm_policies():
             "~/.sds/files/es_ilm_policy_grq.json",
             f"{hysds_dir}/ops/grq2/config/es_ilm_policy_grq.json"
         )
-        run("curl --request PUT --url 'localhost:9200/_ilm/policy/opera_grq_ilm_policy?pretty' --fail-with-body"
+        run("curl --request PUT --url 'localhost:9200/_ilm/policy/opera_grq_ilm_policy?pretty' --fail-with-body "
             f"--json @{hysds_dir}/ops/grq2/config/es_ilm_policy_grq.json")
 
 
