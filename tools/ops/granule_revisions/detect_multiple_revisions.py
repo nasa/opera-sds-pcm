709dc90b45dfb41097e5f93a81a9c7566f3ccedd
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2023-08-09 15:58:28 -0700

    Issue #530: Updated hls_audit.py for no-clobber update

--- tools/ops/granule_revisions/detect_multiple_revisions.py
+++ tools/ops/granule_revisions/detect_multiple_revisions.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
     granules = defaultdict(list)
 
-    s3_prefix = 'inputs/SLC/'
+    s3_prefix = 'inputs/HLS_S30/'
     s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
     paginator = s3.get_paginator('list_objects_v2')
     pages = paginator.paginate(Bucket='opera-dev-rs-fwd-pyoon', Prefix=s3_prefix, Delimiter='/')
@@ -22,9 +22,12 @@ if __name__ == '__main__':
         for obj in page['CommonPrefixes']:
             path = obj.get('Prefix')
             fname = path.split('/')[-2]
-            granule = fname.split('-')[0]
-            revision = fname.split('-')[1]
-            granules[granule].append(revision)
+            try:
+                granule = fname.split('-')[0]
+                revision = fname.split('-')[1]
+                granules[granule].append(revision)
+            except Exception:
+                print(f"{fname} doesn't make sense.")
     #print(granules)
     print(f'{len(granules)} Unique granules found')
     print("Following are granules with more than one revision:")
