5d38f090310fbcded86fdb5af192dc276dc59560
chrisjrd <chris.jrd@gmail.com>
2023-08-15 10:29:08 -0700

    feat(terraform): add missing echo for cloud-init waits
    Refs #576

--- cluster_provisioning/modules/common/mozart.tf
+++ cluster_provisioning/modules/common/mozart.tf
@@ -638,7 +638,7 @@ resource "null_resource" "install_pcm_and_pges" {
 
   provisioner "remote-exec" {
     inline = [<<-EOT
-      while [ ! -f /var/lib/cloud/instance/boot-finished ]; do sleep 5; done
+      while [ ! -f /var/lib/cloud/instance/boot-finished ]; do echo 'Waiting for cloud-init...'; sleep 5; done
       set -ex
       source ~/.bash_profile
 
@@ -676,7 +676,7 @@ resource "null_resource" "install_pcm_and_pges_iems" {
 
   provisioner "remote-exec" {
     inline = [<<-EOT
-      while [ ! -f /var/lib/cloud/instance/boot-finished ]; do sleep 5; done
+      while [ ! -f /var/lib/cloud/instance/boot-finished ]; do echo 'Waiting for cloud-init...'; sleep 5; done
       set -ex
       source ~/.bash_profile
 
@@ -712,7 +712,7 @@ resource "null_resource" "setup_trigger_rules" {
 
   provisioner "remote-exec" {
     inline = [<<-EOT
-      while [ ! -f /var/lib/cloud/instance/boot-finished ]; do sleep 5; done
+      while [ ! -f /var/lib/cloud/instance/boot-finished ]; do echo 'Waiting for cloud-init...'; sleep 5; done
       set -ex
       source ~/.bash_profile
 
