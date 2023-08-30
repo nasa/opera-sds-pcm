91676649fe1f9412f22161ca52092a4914868169
hyunlee <Hyun.H.Lee@jpl.nasa.gov>
2023-08-30 11:44:46 -0700

    hotfix: add poppler==22.12.0 to mozart.tf

--- cluster_provisioning/modules/common/mozart.tf
+++ cluster_provisioning/modules/common/mozart.tf
@@ -382,7 +382,7 @@ resource "aws_instance" "mozart" {
       export PATH=$HOME/conda/bin:$PATH;
       conda-unpack;
       echo installing gdal for manual execution of daac_data_subscriber.py ;
-      conda install conda==22.11.1 gdal==3.6.2 --yes --quiet ;
+      conda install conda==22.11.1 gdal==3.6.2 poppler==22.12.0 --yes --quiet ;
 
       rm -rf hysds-conda_env-${var.hysds_release}.tar.gz
       '
@@ -397,7 +397,7 @@ resource "aws_instance" "mozart" {
         export PATH=$HOME/conda/bin:$PATH
         conda-unpack
         echo installing gdal for manual execution of daac_data_subscriber.py
-        conda install conda==22.11.1 gdal==3.6.2 --yes --quiet
+        conda install conda==22.11.1 gdal==3.6.2 poppler==22.12.0 --yes --quiet
 
         rm -rf hysds-conda_env-${var.hysds_release}.tar.gz
 
