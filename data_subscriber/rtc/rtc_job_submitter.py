a932192f66f4aed06012c1f7dc4f364bca34eee1
chrisjrd <chris.jrd@gmail.com>
2023-11-13 09:58:10 -0800

    refactor(data-subscriber): extract job_submitter module for better future reusability
    Refs #489

--- data_subscriber/rtc/rtc_job_submitter.py
+++ data_subscriber/rtc/rtc_job_submitter.py
@@ -5,8 +5,7 @@ import uuid
 from functools import partial
 from typing import Optional
 
-import backoff
-from hysds_commons.job_utils import submit_mozart_job
+from data_subscriber.rtc.job_submitter import submit_mozart_job
 
 logger = logging.getLogger(__name__)
 
@@ -91,81 +90,13 @@ def create_job_params(product):
         }
     ]
 
-
 def submit_dswx_s1_job(*, product: dict, job_queue: str, rule_name, params: list[dict[str, str]], job_spec: str, job_type: Optional[str] = None, job_name) -> str:
-    return _try_submit_mozart_job_minimal(
-        product=product or {},
-        job_queue=job_queue,
-        rule_name=rule_name,
-        hysdsio={
-            "id": str(uuid.uuid4()),
-            "params": params,
-            "job-specification": job_spec
-        },
-        # job_type=job_type,
-        job_name=job_name
-    )
-
-
-@backoff.on_exception(backoff.expo, exception=Exception, max_tries=3, jitter=None)
-def _try_submit_mozart_job_minimal(*, product: Optional[dict], job_queue: str, rule_name, hysdsio: dict, job_name) -> str:
-    return _submit_mozart_job_minimal(
-        product=product,
-        job_queue=job_queue,
-        rule_name=rule_name,
-        hysdsio=hysdsio,
-        job_name=job_name
-    )
-
-
-def _submit_mozart_job_minimal(*, product: Optional[dict], job_queue: str, rule_name, hysdsio: dict, job_name) -> str:
     return submit_mozart_job(
-        product=product or {},
-        rule={
-            "rule_name": rule_name,
-            "queue": job_queue,
-            "priority": "0",
-            "kwargs": "{}",
-            "enable_dedup": True
-        },
-        hysdsio=hysdsio,
-        queue=None,
-        job_name=job_name,
-        payload_hash=None,
-        enable_dedup=None,
-        soft_time_limit=None,
-        time_limit=None
-    )
-
-
-@backoff.on_exception(backoff.expo, exception=Exception, max_tries=3, jitter=None)
-def _try_submit_mozart_job_by_rule_minimal(*, product: Optional[dict], rule_name, job_type: str, job_queue: str, job_name) -> str:
-    return _submit_mozart_job_by_rule_minimal(
         product=product,
+        job_queue=job_queue,
         rule_name=rule_name,
+        params=params,
+        job_spec=job_spec,
         job_type=job_type,
-        job_queue=job_queue,
         job_name=job_name
-    )
-
-
-def _submit_mozart_job_by_rule_minimal(*, product: Optional[dict], rule_name, job_type: str, job_queue: str, job_name) -> str:
-    return submit_mozart_job(
-        product=product or {},
-        rule={
-            "rule_name": rule_name,
-            "job_type": job_type,
-            "queue": job_queue,
-            "priority": "0",
-            "kwargs": "{}",
-            "enable_dedup": True
-        },
-        hysdsio=None,  # setting None forces lookup based on rule and component.
-        queue=None,
-        job_name=job_name,
-        payload_hash=None,
-        enable_dedup=None,
-        soft_time_limit=None,
-        time_limit=None,
-        component="grq"  # hysds-io information is in the hysds_ios-grq index rather thann hysds_ios-mozart
     )
