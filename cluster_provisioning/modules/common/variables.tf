d29351290100754eac9ee8983cb59c1e846dc6b6
Scott Collins <collinss@jpl.nasa.gov>
2023-11-27 10:37:46 -0800

    Rolled DSWx-S1 version to 3.0.0-er.5.0 in Terraform scripts

--- cluster_provisioning/modules/common/variables.tf
+++ cluster_provisioning/modules/common/variables.tf
@@ -469,8 +469,8 @@ variable "pge_releases" {
     "dswx_hls" = "1.0.2"
     "cslc_s1"  = "2.1.0"
     "rtc_s1"   = "2.1.0"
-    "dswx_s1" = "3.0.0-er.4.0"
-    "disp_s1" = "3.0.0-er.4.0"
+    "dswx_s1"  = "3.0.0-er.5.0"
+    "disp_s1"  = "3.0.0-er.4.0"
   }
 }
 
