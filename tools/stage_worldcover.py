fe2066356044d190e8176bce316a8ac10e4a234a
Scott Collins <collinss@jpl.nasa.gov>
2023-11-28 11:02:49 -0800

    Added fallback logic to stage_worldcover.translate_worldcover()
    When a call to gdal.Translate fails with the "negative width and/or height" error, the function now falls back to translation by the unadjusted projection window.
    This was demonstrated to work around the translation error that occurs for specific MGRS tiles.

--- tools/stage_worldcover.py
+++ tools/stage_worldcover.py
@@ -135,21 +135,38 @@ def translate_worldcover(vrt_filename, output_path, x_min, x_max, y_min, y_max):
                 f"to {output_path}")
     ds = gdal.Open(vrt_filename, gdal.GA_ReadOnly)
 
-    # update cropping coordinates to not exceed the input DEM bounding box
+    # update cropping coordinates to not exceed the input bounding box
     input_x_min, xres, _, input_y_max, _, yres = ds.GetGeoTransform()
     length = ds.GetRasterBand(1).YSize
     width = ds.GetRasterBand(1).XSize
     input_y_min = input_y_max + (length * yres)
     input_x_max = input_x_min + (width * xres)
 
-    x_min = max(x_min, input_x_min)
-    x_max = min(x_max, input_x_max)
-    y_min = max(y_min, input_y_min)
-    y_max = min(y_max, input_y_max)
+    adjusted_x_min = max(x_min, input_x_min)
+    adjusted_x_max = min(x_max, input_x_max)
+    adjusted_y_min = max(y_min, input_y_min)
+    adjusted_y_max = min(y_max, input_y_max)
 
-    gdal.Translate(
-        output_path, ds, format='GTiff', projWin=[x_min, y_max, x_max, y_min]
+    logger.info(
+        f"Adjusted projection window {str([adjusted_x_min, adjusted_y_max, adjusted_x_max, adjusted_y_min])}"
     )
+    try:
+        gdal.Translate(
+            output_path, ds, format='GTiff',
+            projWin=[adjusted_x_min, adjusted_y_max, adjusted_x_max, adjusted_y_min]
+        )
+    except RuntimeError as err:
+        if "negative width and/or height" in str(err):
+            logger.warning(
+                f"Adjusted window translation failed due to negative width and/or "
+                f"height, defaulting to original projection window"
+            )
+            gdal.Translate(
+                output_path, ds, format='GTiff', projWin=[x_min, y_max, x_max, y_min]
+            )
+            return
+
+        raise
 
 
 def download_worldcover(polys, worldcover_bucket, worldcover_ver,
