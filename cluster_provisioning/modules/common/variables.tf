056dc4811fd259d8f626842b5148c519ea1fa936
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2023-11-30 10:38:19 -0800

    Issue #662: CSLC download queue

--- cluster_provisioning/modules/common/variables.tf
+++ cluster_provisioning/modules/common/variables.tf
@@ -444,6 +444,26 @@ variable "queues" {
       "total_jobs_metric" = false
       "use_private_vpc"   = false
     }
+    "opera-job_worker-cslc_data_query" = {
+      "name"              = "opera-job_worker-cslc_data_query"
+      "instance_type"     = ["t2.medium", "t3a.medium", "t3.medium"]
+      "root_dev_size"     = 50
+      "data_dev_size"     = 25
+      "min_size"          = 0
+      "max_size"          = 1
+      "total_jobs_metric" = false
+      "use_private_vpc"   = false
+    }
+    "opera-job_worker-cslc_data_query_hist" = {
+      "name"              = "opera-job_worker-cslc_data_query_hist"
+      "instance_type"     = ["t2.medium", "t3a.medium", "t3.medium"]
+      "root_dev_size"     = 50
+      "data_dev_size"     = 25
+      "min_size"          = 0
+      "max_size"          = 1
+      "total_jobs_metric" = false
+      "use_private_vpc"   = false
+    }
     "opera-job_worker-cslc_data_download" = {
       "name"              = "opera-job_worker-cslc_data_download"
       "instance_type"     = ["c5n.2xlarge", "m5dn.2xlarge"]
