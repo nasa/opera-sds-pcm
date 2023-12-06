1ebb2194fe0a1dcbed55baa5ee87fc58e726740d
chrisjrd <chris.jrd@gmail.com>
2023-12-05 17:53:06 -0800

    feat(data-subscriber): chunk requests
    Refs #489

--- util/aws_util.py
+++ util/aws_util.py
@@ -18,9 +18,7 @@ logger = logging.getLogger(__name__)
 def concurrent_s3_client_try_upload_file(bucket: str, key_prefix: str, files: list[Path]):
     """Upload s3 files concurrently, returning their s3 paths if all succeed."""
     logger.info(f"Uploading {len(files)} files to S3")
-    max_workers = semaphore_size = min(8, os.cpu_count() + 4)
-    logger.info(f"{semaphore_size=}")
-    logger.info(f"{max_workers=}")
+    chunk_size = max_workers = semaphore_size = min(8, os.cpu_count() + 4)
     sem = threading.Semaphore(semaphore_size)
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
         s3pathss = []
