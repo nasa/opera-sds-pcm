8312d5a8cd70228330441725cad57ad542377472
Scott Collins <collinss@jpl.nasa.gov>
2023-12-07 10:55:58 -0800

    Added true implementations for the DSWx-S1 PGE wrapper functions

--- wrapper/pge_functions.py
+++ wrapper/pge_functions.py
@@ -76,22 +76,40 @@ def dswx_hls_lineage_metadata(context, work_dir):
 
 def dswx_s1_lineage_metadata(context, work_dir):
     """Gathers the lineage metadata for the DSWx-S1 PGE"""
+    run_config: Dict = context.get("run_config")
+
     lineage_metadata = []
 
-    rtc_data_dir = os.path.join(work_dir, 'dswx_s1_sample_input_data', 'rtc_data')
+    for s3_input_filepath in run_config["input_file_group"]["input_file_paths"]:
+        local_input_filepath = os.path.join(work_dir, basename(s3_input_filepath))
+        lineage_metadata.append(local_input_filepath)
 
-    # TODO: update paths as necessary as sample inputs are phased out
-    lineage_metadata.extend(
-        [os.path.join(rtc_data_dir, rtc_dir)
-         for rtc_dir in os.listdir(rtc_data_dir)]
+    # Copy the ancillaries downloaded for this job to the pge input directory
+    local_dem_filepaths = glob.glob(os.path.join(work_dir, "dem*.*"))
+    lineage_metadata.extend(local_dem_filepaths)
+
+    local_hand_filepaths = glob.glob(os.path.join(work_dir, "hand_file*.*"))
+    lineage_metadata.extend(local_hand_filepaths)
+
+    local_worldcover_filepaths = glob.glob(os.path.join(work_dir, "worldcover_file*.*"))
+    lineage_metadata.extend(local_worldcover_filepaths)
+
+    local_ref_water_filepaths = glob.glob(os.path.join(work_dir, "reference_water_file*.*"))
+    lineage_metadata.extend(local_ref_water_filepaths)
+
+    local_db_filepaths = glob.glob(os.path.join(work_dir, "*.sqlite*"))
+    lineage_metadata.extend(local_db_filepaths)
+
+    local_algorithm_parameters_filepath = os.path.join(
+        work_dir, basename(run_config["processing"]["algorithm_parameters"])
     )
+    lineage_metadata.append(local_algorithm_parameters_filepath)
 
-    ancillary_data_dir = os.path.join(work_dir, 'dswx_s1_sample_input_data', 'ancillary_data')
-
-    lineage_metadata.extend(
-        [os.path.join(ancillary_data_dir, ancillary)
-         for ancillary in os.listdir(ancillary_data_dir)]
-    )
+    # TODO: enable if shoreline files are needed by DSWx-S1 SAS
+    #shoreline_shape_filename = run_config["dynamic_ancillary_file_group"]["shoreline_shapefile"]
+    #shoreline_shape_basename = splitext(basename(shoreline_shape_filename))[0]
+    #local_shoreline_filepaths = glob.glob(os.path.join(work_dir, f"{shoreline_shape_basename}.*"))
+    #lineage_metadata.extend(local_shoreline_filepaths)
 
     return lineage_metadata
 
@@ -219,20 +237,28 @@ def update_dswx_s1_runconfig(context, work_dir):
 
     container_home: str = container_home_param['value']
     container_home_prefix = f'{container_home}/input_dir'
-    rtc_data_prefix = os.path.join(work_dir, 'dswx_s1_sample_input_data', 'rtc_data')
 
     input_file_paths = run_config["input_file_group"]["input_file_paths"]
-    input_file_paths = list(map(lambda x: x.replace(rtc_data_prefix, container_home_prefix), input_file_paths))
+    updated_input_file_paths = [os.path.join(container_home_prefix, basename(input_file_path))
+                                for input_file_path in input_file_paths]
+    run_config["input_file_group"]["input_file_paths"] = updated_input_file_paths
 
-    run_config["input_file_group"]["input_file_paths"] = input_file_paths
+    dynamic_ancillary_file_paths = run_config["dynamic_ancillary_file_group"]
+    updated_dynamic_ancillary_file_paths = {
+        ancillary_file_type: os.path.join(container_home_prefix, basename(ancillary_file_path))
+        for ancillary_file_type, ancillary_file_path in dynamic_ancillary_file_paths.items()
+    }
+    run_config["dynamic_ancillary_file_group"] = updated_dynamic_ancillary_file_paths
 
-    # TODO update these once we move away from sample inputs
-    run_config["dynamic_ancillary_file_group"]["dem_file"] = f'{container_home_prefix}/dem.tif'
-    run_config["dynamic_ancillary_file_group"]["hand_file"] = f'{container_home_prefix}/hand.tif'
-    run_config["dynamic_ancillary_file_group"]["worldcover_file"] = f'{container_home_prefix}/worldcover.tif'
-    run_config["dynamic_ancillary_file_group"]["reference_water_file"] = f'{container_home_prefix}/reference_water.tif'
+    static_ancillary_file_paths = run_config["static_ancillary_file_group"]
+    updated_static_ancillary_file_paths = {
+        ancillary_file_type: os.path.join(container_home_prefix, basename(ancillary_file_path))
+        for ancillary_file_type, ancillary_file_path in static_ancillary_file_paths.items()
+    }
+    run_config["static_ancillary_file_group"] = updated_static_ancillary_file_paths
 
-    run_config["processing"]["algorithm_parameters"] = f'{container_home_prefix}/algorithm_parameter_s1.yaml'
+    algorithm_parameters_filename = basename(run_config["processing"]["algorithm_parameters"])
+    run_config["processing"]["algorithm_parameters"] = f'{container_home_prefix}/{algorithm_parameters_filename}'
 
     return run_config
 
