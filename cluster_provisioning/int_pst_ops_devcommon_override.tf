9a003dc128b9082f0b6307a50af6027dc5ec1636
hyunlee <Hyun.H.Lee@jpl.nasa.gov>
2023-12-14 16:22:46 -0800

    Update branch version to 3.0.0-er.1.0

--- cluster_provisioning/int_pst_ops_devcommon_override.tf
+++ cluster_provisioning/int_pst_ops_devcommon_override.tf
@@ -6,25 +6,25 @@ variable "hysds_release" {
 }
 
 variable "lambda_package_release" {
-  default = "2.1.0"
+  default = "3.0.0-er.1.0"
 }
 
 variable "pcm_commons_branch" {
-  default = "2.1.0"
+  default = "3.0.0-er.1.0"
 }
 
 variable "pcm_branch" {
-  default = "2.1.0"
+  default = "3.0.0-er.1.0"
 }
 
 variable "product_delivery_branch" {
-  default = "2.1.0"
+  default = "3.0.0-er.1.0"
 }
 
 variable "bach_api_branch" {
-  default = "2.1.0"
+  default = "3.0.0-er.1.0"
 }
 
 variable "bach_ui_branch" {
-  default = "2.1.0"
+  default = "3.0.0-er.1.0"
 }
