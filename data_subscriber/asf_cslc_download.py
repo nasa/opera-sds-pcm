c5ffb8fa66f1e437c054d256b889607e1edc70d6
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2023-12-28 14:35:05 -0800

    Issue #706: Attempt at submitting SCIFLO job. Probably will not work

--- data_subscriber/asf_cslc_download.py
+++ data_subscriber/asf_cslc_download.py
@@ -12,6 +12,7 @@ from data_subscriber.asf_rtc_download import AsfDaacRtcDownload
 from data_subscriber.url import _has_url, _to_url, _to_https_url, _rtc_url_to_chunk_id
 from util.aws_util import concurrent_s3_client_try_upload_file
 from util.conf_util import SettingsConf
+from util.job_submitter import try_submit_mozart_job
 
 logger = logging.getLogger(__name__)
 
@@ -43,4 +44,67 @@ class AsfDaacCslcDownload(AsfDaacRtcDownload):
         s3paths: list[str] = concurrent_s3_client_try_upload_file(bucket=settings["DATASET_BUCKET"],
                                                                   key_prefix=f"tmp/disp_s1/{batch_id}",
                                                                   files=files_to_upload)
+
+
+        # TODO: This code differs from data_subscriber/rtc/rtc_job_submitter.py. Ideally both should be refactored into a common function
+        # Now submit DISP-S1 SCIFLO job
+        logger.info(f"Submitting DISP-S1 SCIFLO job")
+
+        product = {
+            "_id": batch_id,
+            "_source": {
+                "dataset": "dummy_dataset",
+                "metadata": {
+                    "batch_id": batch_id,
+                    "product_paths": {"L2_DISP_S1": s3paths},
+                    "FileName": batch_id,
+                    "id": batch_id,
+                    "bounding_box": None, #TODO: Do we need this?
+                    "Files": [
+                        {
+                            "FileName": PurePath(s3path).name,
+                            "FileSize": 1,
+                            "FileLocation": os.path.dirname(s3path),
+                            "id": PurePath(s3path).name,
+                            "product_paths": "$.product_paths"
+                        }
+                        for s3path in s3paths
+                    ]
+                }
+            }
+        }
+
+        return try_submit_mozart_job(
+            product=product,
+            job_queue=f'opera-job_worker-{"sciflo-l3_disp_s1"}',
+            rule_name=f'trigger-{"SCIFLO_L3_DISP_S1"}',
+            params=self.create_job_params(params),
+            job_spec=f'job-{"SCIFLO_L3_DISP_S1"}:{args.release_version}',
+            job_type=f'hysds-io-{"SCIFLO_L3_DISP_S1"}:{args.release_version}',
+            job_name=f'job-WF-{"SCIFLO_L3_DISP_S1"}'
+        )
+
+    def create_job_params(self, product):
+        return [
+            {
+                "name": "dataset_type",
+                "from": "value",
+                "type": "text",
+                "value": "L2_CSLC_S1"
+            },
+            {
+                "name": "input_dataset_id",
+                "type": "text",
+                "from": "value",
+                "value": product["_source"]["metadata"]["batch_id"]
+            },
+            {
+                "name": "product_metadata",
+                "from": "value",
+                "type": "object",
+                "value": product["_source"]
+            }
+        ]
+
+
 
