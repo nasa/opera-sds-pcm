5359120f9c33ac9cfdd931608b5d98726835f125
Scott Collins <collinss@jpl.nasa.gov>
2024-01-08 10:51:37 -0800

    Trimmed DISP-S1 job parameter template in asf_cslc_download.py to only the overwritten fields

--- data_subscriber/asf_cslc_download.py
+++ data_subscriber/asf_cslc_download.py
@@ -107,6 +107,12 @@ class AsfDaacCslcDownload(AsfDaacRtcDownload):
     def create_job_params(self, product):
         return [
             {
+                "name": "dataset_type",
+                "from": "value",
+                "type": "text",
+                "value": "L2_CSLC_S1"
+            },
+            {
                 "name": "input_dataset_id",
                 "type": "text",
                 "from": "value",
@@ -117,74 +123,5 @@ class AsfDaacCslcDownload(AsfDaacRtcDownload):
                 "from": "value",
                 "type": "object",
                 "value": product["_source"]
-            },
-            {
-                "name": "module_path",
-                "from": "value",
-                "type": "text",
-                "value": "/home/ops/verdi/ops/opera-pcm"
-            },
-            {
-                "name": "wf_dir",
-                "from": "value",
-                "type": "text",
-                "value": "/home/ops/verdi/ops/opera-pcm/opera_chimera/wf_xml"
-            },
-            {
-                "name": "wf_name",
-                "from": "value",
-                "type": "text",
-                "value": "L3_DISP_S1"
-            },
-            {
-                "name": "dataset_type",
-                "from": "value",
-                "type": "text",
-                "value": "L2_CSLC_S1"
-            },
-            {
-                "name": "accountability_module_path",
-                "from": "value",
-                "type": "text",
-                "value": "opera_chimera.accountability"
-            },
-            {
-                "name": "accountability_class",
-                "from": "value",
-                "type": "text",
-                "value": "OperaAccountability"
-            },
-            {
-                "name": "pge_runconfig_dir",
-                "from": "value",
-                "type": "text",
-                "value": "pge_runconfig_dir"
-            },
-            {
-                "name": "pge_input_dir",
-                "from": "value",
-                "type": "text",
-                "value": "pge_input_dir"
-            },
-            {
-                "name": "pge_output_dir",
-                "from": "value",
-                "type": "text",
-                "value": "pge_output_dir"
-            },
-            {
-                "name": "container_home",
-                "from": "value",
-                "type": "text",
-                "value": "/home/mamba"
-            },
-            {
-                "name": "container_working_dir",
-                "from": "value",
-                "type": "text",
-                "value": "/home/mamba"
             }
         ]
-
-
-
