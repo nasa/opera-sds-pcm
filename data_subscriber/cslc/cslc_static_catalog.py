d77123e0e922be0f39fac5d8927b895e1df659ed
Scott Collins <collinss@jpl.nasa.gov>
2024-02-15 14:16:50 -0800

    Added initial implementation of CSLCStaticProductCatalog class

--- data_subscriber/cslc/cslc_static_catalog.py
+++ data_subscriber/cslc/cslc_static_catalog.py
@@ -1,17 +1,14 @@
 import logging
 from datetime import datetime
-from ..hls.hls_catalog import HLSProductCatalog
-from pathlib import Path
-
-from data_subscriber import es_conn_util
+from data_subscriber.hls.hls_catalog import HLSProductCatalog
 
 null_logger = logging.getLogger('dummy')
 null_logger.addHandler(logging.NullHandler())
 null_logger.propagate = False
 
-class SLCProductCatalog(HLSProductCatalog):
+class CSLCStaticProductCatalog(HLSProductCatalog):
     """
-    Class to track products downloaded by daac_data_subscriber.py
+    Class to track CSLC-S1 Static Layer products downloaded by daac_data_subscriber.py
 
     https://github.com/hysds/hysds_commons/blob/develop/hysds_commons/elasticsearch_utils.py
     ElasticsearchUtility methods
@@ -25,15 +22,13 @@ class SLCProductCatalog(HLSProductCatalog):
     """
     def __init__(self, /, logger=None):
         super().__init__(logger=logger)
-        self.ES_INDEX_PATTERNS = "slc_catalog*"
+        self.ES_INDEX_PATTERNS = "cslc_static_catalog*"
 
     def granule_and_revision(self, es_id):
-        '''For S1A_IW_SLC__1SDV_20220601T000522_20220601T000549_043462_05308F_86F3.zip-r5 returns:
-               S1A_IW_SLC__1SDV_20220601T000522_20220601T000549_043462_05308F_86F3-SLC and 5 '''
-        return es_id.split('.zip')[0]+'-SLC', es_id.split('-r')[1]
+        return es_id.split('-r')[0], es_id.split('-r')[1]
 
     def generate_es_index_name(self):
-        return "slc_catalog-{date}".format(date=datetime.utcnow().strftime("%Y.%m"))
+        return "cslc_static_catalog-{date}".format(date=datetime.utcnow().strftime("%Y.%m"))
 
     def filter_query_result(self, query_result):
         return [result['_source'] for result in (query_result or [])]
