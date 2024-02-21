901ce113e14dbb44b9d22f4f95c2ef28a8e53319
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2024-02-21 09:48:12 -0800

    Created opera unit test and added cslc and other query unit tests for now

--- tests/data_subscriber/test_cslc_query.py
+++ tests/data_subscriber/test_cslc_query.py
@@ -7,10 +7,12 @@ import pytest
 from data_subscriber import daac_data_subscriber, query, cslc_utils
 from data_subscriber.cslc import cslc_query
 
-forward_arguments = ["query", "-c", "OPERA_L2_CSLC-S1_V1", "--processing-mode=forward", "--start-date=2021-01-24T23:00:00Z", "--end-date=2021-01-25T00:00:00Z"]
+forward_arguments = ["query", "-c", "OPERA_L2_CSLC-S1_V1", "--processing-mode=forward", "--start-date=2021-01-24T23:00:00Z",\
+                     "--end-date=2021-01-25T00:00:00Z", "--grace-mins=60"]
 forward_args = daac_data_subscriber.create_parser().parse_args(forward_arguments)
 
-hist_arguments = ["query", "-c", "OPERA_L2_CSLC-S1_V1", "--processing-mode=historical", "--start-date=2021-01-24T23:00:00Z", "--end-date=2021-01-24T23:00:00Z", "--frame-range=100,101"]
+hist_arguments = ["query", "-c", "OPERA_L2_CSLC-S1_V1", "--processing-mode=historical", "--start-date=2021-01-24T23:00:00Z",\
+                  "--end-date=2021-01-24T23:00:00Z", "--frame-range=100,101"]
 hist_args = daac_data_subscriber.create_parser().parse_args(hist_arguments)
 
 disp_burst_map, burst_to_frame, metadata, version = cslc_utils.process_disp_frame_burst_json(cslc_utils.DISP_FRAME_BURST_MAP_JSON)
