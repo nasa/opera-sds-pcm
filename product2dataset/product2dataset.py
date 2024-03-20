1092cefd3771ed3b99436985e4c784bea35d2bdd
chrisjrd <chris.jrd@gmail.com>
2024-03-20 15:30:19 -0700

    fix(accountability): product received time
    Refs #770

--- product2dataset/product2dataset.py
+++ product2dataset/product2dataset.py
@@ -162,6 +162,8 @@ def convert(
             dataset_met_json["orbit_file"] = PurePath(extra_met["runconfig"]["localize"][0]).name
         elif pge_name == "L3_DSWx_S1":
             dataset_met_json["input_granule_id"] = product_metadata["id"]
+            if product_metadata.get("ProductReceivedTime"):
+                dataset_met_json["InputProductReceivedTime"] = product_metadata["ProductReceivedTime"]
         elif pge_name == "L3_DISP_S1":
             dataset_met_json["input_granule_id"] = product_metadata["id"]
 
