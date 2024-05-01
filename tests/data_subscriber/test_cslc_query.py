14eab7cc1317a7976fb75584172ee998dcc9a1c9
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2024-05-01 15:40:17 -0700

    820: DISP-S1 processing query in reprocessing mode can now use date range as input. Fixed issue with negative acquisition time cycle

--- tests/data_subscriber/test_cslc_query.py
+++ tests/data_subscriber/test_cslc_query.py
@@ -5,6 +5,8 @@ import pytest
 from data_subscriber import cslc_utils
 from data_subscriber.parser import create_parser
 from data_subscriber.cslc import cslc_query
+from datetime import datetime
+from data_subscriber.query import DateTimeRange
 
 forward_arguments = ["query", "-c", "OPERA_L2_CSLC-S1_V1", "--processing-mode=forward", "--start-date=2021-01-24T23:00:00Z",\
                      "--end-date=2021-01-25T00:00:00Z", "--grace-mins=60"]
@@ -14,8 +16,8 @@ hist_arguments = ["query", "-c", "OPERA_L2_CSLC-S1_V1", "--processing-mode=histo
                   "--end-date=2021-01-24T23:00:00Z", "--frame-range=100,101"]
 hist_args = create_parser().parse_args(hist_arguments)
 
-disp_burst_map, burst_to_frame, metadata, version = cslc_utils.localize_disp_frame_burst_json(cslc_utils.DISP_FRAME_BURST_MAP_JSON)
-disp_burst_map_hist = cslc_utils.localize_disp_frame_burst_hist(cslc_utils.DISP_FRAME_BURST_MAP_HIST)
+disp_burst_map, burst_to_frame, metadata, version = cslc_utils.process_disp_frame_burst_json(cslc_utils.DISP_FRAME_BURST_MAP_JSON)
+disp_burst_map_hist = cslc_utils.process_disp_frame_burst_hist(cslc_utils.DISP_FRAME_BURST_MAP_HIST)
 
 @pytest.mark.skip
 def test_frame_range():
@@ -77,6 +79,37 @@ def test_extend_additional_records():
     c_query.extend_additional_records(granules)
 
     assert len(granules) == 5
+
+@pytest.mark.asyncio
+async def test_reprocessing_by_dates():
+    ''' Tests reprocessing query commands and high-level processing'''
+    reprocessing_arguments = ["query", "-c", "OPERA_L2_CSLC-S1_V1", "--processing-mode=reprocessing",
+                              "--start-date=2021-01-24T23:00:00Z", "--end-date=2021-01-25T00:00:00Z",
+                              "--k=4", "--no-schedule-download"]
+    reproc_args = create_parser().parse_args(reprocessing_arguments)
+    query_timerange = DateTimeRange(reproc_args.start_date, reproc_args.end_date)
+    c_query = cslc_query.CslcCmrQuery(reproc_args, None, None, None, None,
+                                      {"DEFAULT_DISP_S1_QUERY_GRACE_PERIOD_MINUTES": 60},
+                                      cslc_utils.DISP_FRAME_BURST_MAP_JSON, cslc_utils.DISP_FRAME_BURST_MAP_HIST)
+    cr = await c_query.query_cmr(reproc_args, None, None, None, query_timerange, datetime.utcnow())
+    args = cr.cr_frame.f_locals["args"]
+    assert args.collection == 'OPERA_L2_CSLC-S1_V1'
+    assert args.start_date == '2021-01-24T23:00:00Z'
+    assert args.end_date == '2021-01-25T00:00:00Z'
+    assert args.proc_mode == 'reprocessing'
+
+@pytest.mark.asyncio
+async def test_reprocessing_by_native_id(caplog):
+    ''' Tests reprocessing query commands and high-level processing when specifying a native_id'''
+    reprocessing_arguments = ["query", "-c", "OPERA_L2_CSLC-S1_V1", "--processing-mode=reprocessing", "--k=4",
+                              "--native-id=OPERA_L2_CSLC-S1_T027-056778-IW1_20231008T133102Z_20231009T204457Z_S1A_VV_v1.0", "--no-schedule-download"]
+    reproc_args = create_parser().parse_args(reprocessing_arguments)
+    c_query = cslc_query.CslcCmrQuery(reproc_args, None, None, None, None,
+                                      {"DEFAULT_DISP_S1_QUERY_GRACE_PERIOD_MINUTES": 60},
+                                      cslc_utils.DISP_FRAME_BURST_MAP_JSON, cslc_utils.DISP_FRAME_BURST_MAP_HIST)
+    cr = await c_query.query_cmr(reproc_args, None, None, None, None, datetime.utcnow())
+    assert 'OPERA_L2_CSLC-S1_T027-056777-IW2' in caplog.text
+
 
 def test_download_batch_id():
     """Test that the download batch id is correctly constructed for forward processing mode"""
