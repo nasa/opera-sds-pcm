6f2080162d2ff37245258145f99bce901e08bec7
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2024-08-21 11:37:18 -0700

    #927: Removed LTS deletion from terraform on most venues and instead added the line in the destroy shell scripts

--- cluster_provisioning/pst/destroy-pst.sh
+++ cluster_provisioning/pst/destroy-pst.sh
@@ -1,3 +1,7 @@
+# Comment the line below if you want to keep the content of LTS bucket which are mainly for Compressed CSLC files
+# Generally you do not want to delete them in the PST venue
+#aws s3 rm --recursive s3://opera-pst-lts-pop1 &&
+
 aws-login -pub &&
 ~/terraform destroy -var-file=pst.tfvars &&
 aws --profile saml-pub logs describe-log-groups --log-group-name-prefix /aws/lambda/opera-pst-pop1 | grep logGroupName | cut -d '"' -f4 | xargs -i -t aws --profile saml-pub logs delete-log-group --log-group-name {} &&
