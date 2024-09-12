2ae01a993bf4c28a53ee2c8748ad0511bdb2e3a9
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2024-09-12 11:00:57 -0700

    Fixed automatic lts compressed file deletion of various venues upon cluster destroying

--- cluster_provisioning/int/int-pop1/destroy-int-pop1.sh
+++ cluster_provisioning/int/int-pop1/destroy-int-pop1.sh
@@ -1,9 +1,8 @@
-# Comment the line below if you want to keep the content of LTS bucket which are mainly for Compressed CSLC files
-aws s3 rm --recursive s3://opera-int-lts-pop1 &&
-
 aws-login -pub &&
 ~/terraform destroy -var-file=int.tfvars &&
 aws --profile saml-pub logs describe-log-groups --log-group-name-prefix /aws/lambda/opera-int-pop1 | grep logGroupName | cut -d '"' -f4 | xargs -i -t aws --profile saml-pub logs delete-log-group --log-group-name {} &&
 aws --profile saml-pub logs describe-log-groups --log-group-name-prefix /opera/sds/opera-int-pop1 | grep logGroupName | cut -d '"' -f4 | xargs -i -t aws --profile saml-pub logs delete-log-group --log-group-name {} &&
 aws --profile saml-pub ec2 describe-launch-templates | grep LaunchTemplateName | grep int-pop1 | cut -d '"' -f4 | xargs -i -t aws --profile saml-pub ec2 delete-launch-template --launch-template-name {}
 
+# Comment the line below if you want to keep the content of LTS bucket which are mainly for Compressed CSLC files
+aws --profile saml-pub s3 rm --recursive s3://opera-int-lts-pop1/products/CSLC_S1_COMPRESSED
