79e0ffecf32e9dff80f824790dd644888b43fe7e
chrisjrd <chris.jrd@gmail.com>
2024-10-03 15:19:39 -0700

    feat(opensearch): adding aws-requests-auth package reference for backwards compatibility with elasticsearch in es_connection.py
    Refs #806

--- setup.py
+++ setup.py
@@ -45,6 +45,8 @@ setup(
 
             "boto3-stubs",
             "boto3-stubs-lite[essential]",  # for ec2, s3, rds, lambda, sqs, dynamo and cloudformation
+
+            "aws-requests-auth",
         ],
         "subscriber": [
             # The list of dependencies required to run the data_subscriber module standalone.
