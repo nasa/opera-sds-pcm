7ff22d17bb7fc53631194fd412b0dce48247f38b
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2024-10-11 10:43:39 -0700

    #1001: Addeded a couple more DISP-S1 scenario tests into the script

--- tests/scenarios/run_all_cslc_query_scenario_tests.sh
+++ tests/scenarios/run_all_cslc_query_scenario_tests.sh
@@ -7,4 +7,6 @@ python cslc_query_test.py cslc_query_reproc_k4_test.json --clear true &&
 python cslc_query_test.py cslc_query_reproc_dates_frameid_k4_test.json --clear true &&
 python cslc_query_test.py cslc_query_fwd_k2_test.json --clear true &&
 python cslc_query_test.py cslc_query_reproc_blackout.json --clear true &&
-python cslc_query_test.py cslc_query_reproc_blackout_empty.json --clear true
+python cslc_query_test.py cslc_query_reproc_blackout_empty.json --clear true &&
+python cslc_query_test.py cslc_query_fwd_blackout_empty_test.json --clear true &&
+python cslc_query_test.py cslc_query_fwd_blackout_test.json --clear true
