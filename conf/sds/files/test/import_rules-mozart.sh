69d598a7f0d770c56192d42b619ba8bc545cd85e
chrisjrd <chris.jrd@gmail.com>
2024-10-18 16:40:44 -0700

    feat(opensearch): update ES connection utils. update import rules.
    Refs #806

--- conf/sds/files/test/import_rules-mozart.sh
+++ conf/sds/files/test/import_rules-mozart.sh
@@ -13,7 +13,13 @@ elif [ ${#SDS_PKGS[@]} -eq 0 ]; then
 fi
 
 # extract version
-MOZART_ES_URL="http://$(grep ^MOZART_ES_PVT_IP ~/.sds/config | awk '{print $2}'):9200"
+MOZART_ES_PVT_IP=$(grep ^MOZART_ES_PVT_IP ~/.sds/config | awk '{print $2}')
+if [ -z "${MOZART_ES_PVT_IP}" ]
+then
+  MOZART_ES_PVT_IP=$(grep -A1 ^MOZART_ES_PVT_IP ~/.sds/config | tail -n 1 | awk '{print $2}')
+fi
+
+MOZART_ES_URL="http://${MOZART_ES_PVT_IP}:9200"
 VERSION=$(curl -s -XGET $MOZART_ES_URL/containers/_doc/${SDS_PKGS[0]}?_source=version | python -c "import json,sys;obj=json.load(sys.stdin);print(obj['_source']['version']);")
 
 # write out rules import
--- data_subscriber/es_conn_util.py
+++ data_subscriber/es_conn_util.py
@@ -1,7 +1,8 @@
-from aws_requests_auth.boto_utils import BotoAWSRequestsAuth
-from elasticsearch import RequestsHttpConnection
 from hysds.celery import app
-from hysds_commons.elasticsearch_utils import ElasticsearchUtility
+from pcm_commons.query.ancillary_utility import AncillaryUtility
+
+from commons.constants import product_metadata
+from commons.logger import logger as default_logger
 
 CONN = None
 
@@ -10,25 +11,57 @@ def get_es_connection(logger):
     global CONN
 
     if CONN is None:
-        aws_es = app.conf['GRQ_AWS_ES']
-        es_host = app.conf['GRQ_ES_HOST']
-        es_url = app.conf['GRQ_ES_URL']
-        region = app.conf['AWS_REGION']
+        aws_es = app.conf.get('GRQ_AWS_ES', False)
+        es_url = app.conf.get('GRQ_ES_URL', "http://localhost:9200")
 
-        if aws_es is True:
-            aws_auth = BotoAWSRequestsAuth(aws_host=es_host, aws_region=region, aws_service='es')
-            CONN = ElasticsearchUtility(
-                es_url=es_url,
-                logger=logger,
-                http_auth=aws_auth,
-                connection_class=RequestsHttpConnection,
-                use_ssl=True,
-                verify_certs=False,
-                ssl_show_warn=False,
-                timeout=30,
-                max_retries=10,
-                retry_on_timeout=True,
-            )
+        es_engine = app.conf.get('GRQ_ES_ENGINE', "elasticsearch")
+
+        if aws_es is True or "es.amazonaws.com" in es_url:
+            region = app.conf.get('AWS_REGION', "us-west-2")
+            if es_engine == "elasticsearch":
+                from aws_requests_auth.boto_utils import BotoAWSRequestsAuth
+                from elasticsearch import RequestsHttpConnection
+
+                es_host = app.conf.get('GRQ_ES_HOST', "localhost:9200")
+                aws_auth = BotoAWSRequestsAuth(aws_host=es_host, aws_region=region, aws_service='es')
+                CONN = AncillaryUtility(
+                    es_url=es_url,
+                    logger=logger,
+                    product_metadata=product_metadata,
+                    http_auth=aws_auth,
+                    connection_class=RequestsHttpConnection,
+                    use_ssl=True,
+                    verify_certs=False,
+                    ssl_show_warn=False,
+                    timeout=30,
+                    max_retries=10,
+                    retry_on_timeout=True,
+                )
+            elif es_engine == "opensearch":
+                import boto3
+                from opensearchpy import AWSV4SignerAuth, RequestsHttpConnection
+
+                credentials = boto3.Session().get_credentials()
+                aws_auth = AWSV4SignerAuth(credentials, region)
+                CONN = AncillaryUtility(
+                    es_url=es_url,
+                    engine=es_engine,
+                    logger=logger or default_logger,
+                    product_metadata=product_metadata,
+                    http_auth=aws_auth,
+                    retry_on_timeout = True,
+                )
         else:
-            CONN = ElasticsearchUtility(es_url, logger)
+            if es_engine == "elasticsearch":
+                CONN = AncillaryUtility(es_url, logger=logger, product_metadata=product_metadata)
+            elif es_engine == "opensearch":
+                CONN = AncillaryUtility(
+                    es_url,
+                    engine=es_engine,
+                    logger=logger or default_logger,
+                    product_metadata=product_metadata,
+                    timeout=30,
+                    max_retries=10,
+                    retry_on_timeout=True,
+                )
     return CONN
