0bb3e970c4e8c9b56264f5e9559532367b6069c9
Scott Collins <collinss@jpl.nasa.gov>
2024-12-03 12:47:02 -0800

    Removed additional unused files

--- conf/sds/files/test/stage_l3_hls_to_s3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-$ ingest L3_DSWx_HLS_PGE outputs
-for file in ~/mozart/ops/opera-pcm/tests/L3_DSWx_HLS_PGE/test-files/expected_outputs/*
-do
-  echo "$file"
-  aws s3 cp $file s3://$1/
-done
-
