9f446cc2676f9b1b804527ecdf3f79997b765354
rileykk <rileykk@jpl.nasa.gov>
2025-03-11 07:45:15 -0700

    DIST-S1: Update simulation filenames

--- tests/unit/util/test_pge_util.py
+++ tests/unit/util/test_pge_util.py
@@ -474,17 +474,18 @@ def test_simulate_dist_s1_pge():
     )
 
     creation_ts = pge_util.get_time_for_filename()
-    expected_output_basename = 'OPERA_L3_DIST-S1_{tile_id}_{creation_ts}Z_{creation_ts}Z_S1A_30_v0.0.1'
-    expected_ancillary_basename = 'OPERA_L3_DIST-S1_{creation_ts}Z_S1A_30_v0.0.1'
+    expected_output_basename = 'OPERA_L3_DIST-ALERT-S1_{tile_id}_{creation_ts}Z_{creation_ts}Z_S1_30_v0.1'
+    expected_ancillary_basename = 'OPERA_L3_DIST-S1_{creation_ts}Z_S1_30_v0.1'
 
     try:
         for tile_id in pge_util.SIMULATED_MGRS_TILES:
             for band in pge_util.DIST_S1_BAND_NAMES:
                 assert Path(f'/tmp/{expected_output_basename.format(tile_id=tile_id, creation_ts=creation_ts)}_{band}.tif').exists()
 
-            assert Path(f'/tmp/{expected_output_basename.format(tile_id=tile_id, creation_ts=creation_ts)}_BROWSE.tif').exists()
-            assert Path(f'/tmp/{expected_output_basename.format(tile_id=tile_id, creation_ts=creation_ts)}_BROWSE.png').exists()
-            # assert Path(f'/tmp/{expected_output_basename.format(tile_id=tile_id, creation_ts=creation_ts)}.iso.xml').exists()
+            # TODO: Current release doesn't make GeoTIFF browse images
+            # assert Path(f'/tmp/{expected_output_basename.format(tile_id=tile_id, creation_ts=creation_ts)}_BROWSE.tif').exists()
+            assert Path(f'/tmp/{expected_output_basename.format(tile_id=tile_id, creation_ts=creation_ts)}.png').exists()
+            assert Path(f'/tmp/{expected_output_basename.format(tile_id=tile_id, creation_ts=creation_ts)}.iso.xml').exists()
 
         assert Path(f'/tmp/{expected_ancillary_basename.format(creation_ts=creation_ts)}.catalog.json').exists()
         assert Path(f'/tmp/{expected_ancillary_basename.format(creation_ts=creation_ts)}.log').exists()
