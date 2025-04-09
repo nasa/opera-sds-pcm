3f4de7be09fd1739fe7815eb9542f50fab73c5ea
rileykk <rileykk@jpl.nasa.gov>
2025-04-09 09:57:38 -0700

    DISP-S1-STATIC wrapper functions

--- wrapper/pge_functions.py
+++ wrapper/pge_functions.py
@@ -189,6 +189,46 @@ def disp_s1_lineage_metadata(context, work_dir):
     return lineage_metadata
 
 
+def disp_s1_static_lineage_metadata(context, work_dir):
+    """Gathers the lineage metadata for the DISP-S1-STATIC PGE"""
+    run_config: Dict = context.get("run_config")
+
+    lineage_metadata = []
+
+    input_file_group = run_config["input_file_group"]
+    s3_input_filepaths = input_file_group["input_file_paths"]
+
+    # Reassign all S3 URI's in the runconfig to where the files now reside on the local worker
+    for s3_input_filepath in s3_input_filepaths:
+        local_input_filepath = os.path.join(work_dir, basename(s3_input_filepath))
+
+        if os.path.isdir(local_input_filepath):
+            lineage_metadata.extend(
+                [os.path.join(local_input_filepath, file_name)
+                 for file_name in os.listdir(local_input_filepath)
+                 if file_name.endswith(".h5")]
+            )
+        else:
+            lineage_metadata.append(local_input_filepath)
+
+    for dynamic_ancillary_key in ("rtc_static_layers_files",):
+        if dynamic_ancillary_key in run_config["dynamic_ancillary_file_group"]:
+            for s3_input_filepath in run_config["dynamic_ancillary_file_group"][dynamic_ancillary_key]:
+                local_input_filepath = os.path.join(work_dir, basename(s3_input_filepath))
+                lineage_metadata.append(local_input_filepath)
+
+    # Copy the pre-downloaded ancillaries for this job to the pge input directory
+    local_dem_filepaths = glob.glob(os.path.join(work_dir, "dem*.*"))
+    lineage_metadata.extend(local_dem_filepaths)
+
+    local_frame_database_filepath = os.path.join(
+        work_dir, basename(run_config["static_ancillary_file_group"]["frame_to_burst_json"])
+    )
+    lineage_metadata.append(local_frame_database_filepath)
+
+    return lineage_metadata
+
+
 def dist_s1_lineage_metadata(context, work_dir):
     """Gathers the lineage metadata for the DIST-S1 PGE"""
     run_config: Dict = context.get("run_config")
@@ -407,6 +447,52 @@ def update_disp_s1_runconfig(context, work_dir):
     run_config["processing"]["algorithm_parameters"] = (
         os.path.join(container_home_prefix, os.path.basename(run_config["processing"]["algorithm_parameters"]))
     )
+
+    return run_config
+
+
+def update_disp_s1_static_runconfig(context, work_dir):
+    """Updates a runconfig for use with the DISP-S1-STATIC PGE"""
+    run_config: Dict = context.get("run_config")
+    job_spec: Dict = context.get("job_specification")
+
+    container_home_param = list(
+        filter(lambda param: param['name'] == 'container_home', job_spec['params'])
+    )[0]
+
+    container_home: str = container_home_param['value']
+    container_home_prefix = f'{container_home}/input_dir'
+
+    local_input_dir = os.path.join(work_dir, "disp_s1_static_r6.6_calval_expected_input", 'cslc_static')
+
+    updated_input_file_paths = []
+
+    for input_file_path in glob.glob(os.path.join(local_input_dir, "*CSLC-S1_*.h5")):
+        updated_input_file_paths.append(os.path.join(container_home_prefix, basename(input_file_path)))
+
+    run_config["input_file_group"]["input_file_paths"] = updated_input_file_paths
+
+    dynamic_ancillary_file_group = run_config["dynamic_ancillary_file_group"]
+
+    for dynamic_ancillary_key in ("rtc_static_layers_files",):
+        if dynamic_ancillary_key in dynamic_ancillary_file_group:
+            dynamic_ancillary_file_group[dynamic_ancillary_key] = [
+                os.path.join(container_home_prefix, basename(input_file_path))
+                for input_file_path in dynamic_ancillary_file_group[dynamic_ancillary_key]
+            ]
+
+    static_ancillary_file_group = run_config["static_ancillary_file_group"]
+
+    for static_ancillary_key in ("frame_to_burst_json",):
+        static_ancillary_file_group[static_ancillary_key] = os.path.join(
+            container_home_prefix, basename(static_ancillary_file_group[static_ancillary_key])
+        )
+
+    if "dem_file" in run_config["dynamic_ancillary_file_group"]:
+        run_config["dynamic_ancillary_file_group"]["dem_file"] = (
+            os.path.join(container_home_prefix,
+                         os.path.basename(run_config["dynamic_ancillary_file_group"]["dem_file"]))
+        )
 
     return run_config
 
