b62495e54730deb1484373f1e09b98b2ed820d32
Philip Yoon <philip.j.yoon@jpl.nasa.gov>
2025-04-08 18:13:57 -0700

    Improvement to DISP-S1 status html renderer. Dependency installation

--- tools/ops/disp_s1_status/disp_s1_hist_status_render.py
+++ tools/ops/disp_s1_status/disp_s1_hist_status_render.py
@@ -4,9 +4,25 @@ import sys
 
 # Render the updated geojson as an image with the map in the background using folium
 import folium
+import branca
+
+colormap = branca.colormap.LinearColormap(
+    vmin=0,
+    vmax=100,
+    colors=["red", "orange", "lightblue", "green", "darkgreen"],
+    caption="DISP-S1 Processing Completion (%)",
+)
 
 with open(sys.argv[1]) as f:
     data = json.load(f)
+
+with open(sys.argv[1]) as f:
+    asc_data = json.load(f)
+asc_data['features'] = [] # We use this so that we can skip the unprocessed frames from cluttering the map and layer control
+
+with open(sys.argv[1]) as f:
+    des_data = json.load(f)
+des_data['features'] = [] # We use this so that we can skip the unprocessed frames from cluttering the map and layer control
 
 m = folium.Map(location=[0, 0], zoom_start=2)
 for feature in data['features']:
@@ -30,20 +46,35 @@ for feature in data['features']:
         else:
             status['color'] = 'yellow'
             status['fillOpacity'] = 0.8
-        feature['properties']['completion_percentage'] = str(status['completion_percentage'])+"%"
+        feature['properties']['completion_percentage'] = status['completion_percentage']
+        feature['properties']['completion_percentage_str'] = str(status['completion_percentage'])+"%"
         feature['properties']['sensing_datetimes_triggered'] = status['sensing_datetimes_triggered']
         feature['properties']['possible_sending_datetimes_to_trigger'] = status['possible_sending_datetimes_to_trigger']
         feature['properties']['sensing_datetime_count'] = status['sensing_datetime_count']
         feature['properties']['last_triggered_sensing_datetime'] = status['last_triggered_sensing_datetime']
 
-popup = folium.GeoJsonTooltip(fields=['frame_id', 'completion_percentage', 'sensing_datetimes_triggered', 'possible_sending_datetimes_to_trigger', 'sensing_datetime_count', 'last_triggered_sensing_datetime'],
+        if feature['properties']['orbit_pass'] == "DESCENDING":
+            des_data['features'].append(feature)
+        elif feature['properties']['orbit_pass'] == "ASCENDING":
+            asc_data['features'].append(feature)
+        else:
+            raise ValueError(f"Unknown orbit pass: {feature['properties']['orbit_pass']}")
+
+    feature['properties']['frame_id'] = str(feature['properties']['frame_id']) # So that we get rid of the comma in the number
+
+popup = folium.GeoJsonTooltip(fields=['frame_id', 'completion_percentage_str', 'sensing_datetimes_triggered', 'possible_sending_datetimes_to_trigger', 'sensing_datetime_count', 'last_triggered_sensing_datetime'],
                             aliases=['Frame ID', 'Completion Percentage', 'Sensing Datetimes Triggered', 'Possible Sensing Datetimes to Trigger', 'Sensing Datetime Count', 'Last Triggered Sensing Datetime'],
                             localize=True,
                             style="width: 300px")
 
-folium.GeoJson(data, style_function=
+popup2 = folium.GeoJsonTooltip(fields=['frame_id', 'completion_percentage_str', 'sensing_datetimes_triggered', 'possible_sending_datetimes_to_trigger', 'sensing_datetime_count', 'last_triggered_sensing_datetime'],
+                            aliases=['Frame ID', 'Completion Percentage', 'Sensing Datetimes Triggered', 'Possible Sensing Datetimes to Trigger', 'Sensing Datetime Count', 'Last Triggered Sensing Datetime'],
+                            localize=True,
+                            style="width: 300px")
+
+folium.GeoJson(asc_data, name = "ASCENDING", style_function=
     lambda x:
-    {'fillColor': x['processing_status']['color'],
+    {'fillColor': colormap(x["properties"]["completion_percentage"]),
      'line_opacity': 0.01,
      "fillOpacity": x['processing_status']['fillOpacity'],
      'color': x['processing_status']['color'],
@@ -51,5 +82,20 @@ folium.GeoJson(data, style_function=
      'weight': 1},
      tooltip= popup
                ).add_to(m)
+
+folium.GeoJson(des_data, name="DESCENDING",  style_function=
+    lambda x:
+    {'fillColor': colormap(x["properties"]["completion_percentage"]),
+     'line_opacity': 0.01,
+     "fillOpacity": x['processing_status']['fillOpacity'],
+     'color': x['processing_status']['color'],
+     'opacity': 0.01,
+     'weight': 1},
+     tooltip= popup2
+               ).add_to(m)
+
+folium.LayerControl().add_to(m)
+colormap.add_to(m)
+
 filename = sys.argv[2]
 m.save(filename)
