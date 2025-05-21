7076456fd774087de4a0f04c551d3439f213beb4
rileykk <rileykk@jpl.nasa.gov>
2025-05-21 13:28:45 -0700

    [DWWx-NI ER4.0] Update terraform

--- cluster_provisioning/modules/common/variables.tf
+++ cluster_provisioning/modules/common/variables.tf
@@ -705,7 +705,7 @@ variable "pge_releases" {
     "rtc_s1"   = "2.1.1"
     "dswx_s1"  = "3.0.2"
     "disp_s1"  = "3.0.6"
-    "dswx_ni"  = "4.0.0-er.3.0"
+    "dswx_ni"  = "4.0.0-er.4.0"
     "dist_s1"  = "6.0.0-er.3.0"
   }
 }
