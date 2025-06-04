e1873e425d0d877ebd97fb8980c0d4d92979a0db
hyunlee <Hyun.H.Lee@jpl.nasa.gov>
2025-06-04 12:53:13 -0700

    Fix deployment error

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="opera_pcm",
-    version="3.2.0-rc.1.0",
+    version="3.1.6",
     packages=find_packages(),
     install_requires=[
         "smart_open",
