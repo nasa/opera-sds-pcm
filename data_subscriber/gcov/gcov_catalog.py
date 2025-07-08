a3777ee072f524c3c68b9f8ff8e8fe95def3ce96
Ryan Hunter <ryan.g.hunter@jpl.nasa.gov>
2025-07-07 17:34:14 -0700

    update gcov catalog for simplicity

--- data_subscriber/gcov/gcov_catalog.py
+++ data_subscriber/gcov/gcov_catalog.py
@@ -1,13 +1,24 @@
+from datetime import datetime
 
 from data_subscriber.catalog import ProductCatalog
 
+
 class NisarGcovProductCatalog(ProductCatalog):
-    """Cataloging class for downloaded NISAR GCOV Products."""
+    """Cataloging class for NISAR GCOV Products to support DSWx-NI triggering."""
     NAME = "nisar_gcov_catalog"
     ES_INDEX_PATTERNS = "nisar_gcov_catalog*"
 
-    def process_query_result(self, query_result):
-        return [result['_source'] for result in (query_result or [])]
+    def get_download_granule_revision(self, download_batch_id: str):
+        return super().get_download_granule_revision(download_batch_id)
 
-    def granule_and_revision(self, es_id):
-        return es_id.split('-r')[0], es_id.split('-r')[1]
+    def form_document(self, filename: str, granule: dict, job_id: str, query_dt: datetime,
+                      temporal_extent_beginning_dt: datetime, revision_date_dt: datetime, revision_id):
+
+        m = super().form_document(
+            filename, granule, job_id, query_dt, temporal_extent_beginning_dt, revision_date_dt, revision_id
+        )
+
+        # Add http_urls and s3_urls to the document
+        m["filtered_urls"] = granule.get("filtered_urls", [])
+
+        return m
