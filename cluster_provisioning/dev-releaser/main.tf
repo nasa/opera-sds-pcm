85fae7c9fbae6751bc2c506fd4de6e5704337fca
hyunlee <Hyun.H.Lee@jpl.nasa.gov>
2025-07-17 15:17:10 -0700

    Use shared_credentials_files instead from shared_credentials_file

--- cluster_provisioning/dev-releaser/main.tf
+++ cluster_provisioning/dev-releaser/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  shared_credentials_file = var.shared_credentials_file
+  shared_credentials_files = [var.shared_credentials_file]
   region                  = var.region
   profile                 = var.profile
 }
