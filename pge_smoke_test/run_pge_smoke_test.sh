c4ade5a52639742a0ee8e764746803fb22d025cf
rileykk <rileykk@jpl.nasa.gov>
2025-08-12 07:33:16 -0700

    [DISP-NI 6.0.0-ER.1.0] smoke test debug stuff

--- pge_smoke_test/run_pge_smoke_test.sh
+++ pge_smoke_test/run_pge_smoke_test.sh
@@ -25,6 +25,9 @@ date 2>&1
 echo "User is $(id -u):$(id -g)"
 echo "Working dir is ${WORKING_DIR}" 2>&1
 
+# TODO deleteme debug
+docker images
+
 VERSION_TAG=$(docker images | grep opera_pge/${PGE_NAME} -m 1 | xargs sh -c 'echo $1') 2>&1
 STATUS=$?
 
