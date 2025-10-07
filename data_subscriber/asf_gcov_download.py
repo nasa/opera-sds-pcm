280e67e08edc4db159a806ae4d061e8e6680f0e8
chrisjrd <chris.jrd@gmail.com>
2025-10-07 14:28:03 -0700

    fix(DSWx-NI): gcov download reference to sciflo job queue. hard-coded.

--- data_subscriber/asf_gcov_download.py
+++ data_subscriber/asf_gcov_download.py
@@ -81,9 +81,12 @@ class AsfDaacGcovDownload(AsfDaacRtcDownload):
         }]
 
     def trigger_dswx_ni_jobs(self, sets_to_process, args, settings):
-        return [submit_dswx_ni_job(
-            params=self.create_dswx_ni_job_params(set_to_process),
-            job_queue=args.job_queue,
-            job_name=f"job-WF-SCIFLO_L3_DSWx_NI-{set_to_process.mgrs_set_id}-{set_to_process.cycle_number}",
-            release_version=settings["RELEASE_VERSION"]
-        ) for set_to_process in sets_to_process]
+        return [
+            submit_dswx_ni_job(
+                params=self.create_dswx_ni_job_params(set_to_process),
+                job_queue=f'opera-job_worker-{"sciflo-l3_dswx_ni"}',
+                job_name=f"job-WF-SCIFLO_L3_DSWx_NI-{set_to_process.mgrs_set_id}-{set_to_process.cycle_number}",
+                release_version=settings["RELEASE_VERSION"]
+            )
+            for set_to_process in sets_to_process
+        ]
