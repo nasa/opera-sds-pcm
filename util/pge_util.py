de2a31ace0406c7f2a215bc525c455df28754d1d
rileykk <rileykk@jpl.nasa.gov>
2025-10-07 10:25:10 -0700

    change debug line

--- util/pge_util.py
+++ util/pge_util.py
@@ -262,7 +262,7 @@ def simulate_run_pge(runconfig: Dict, pge_config: Dict, context: Dict, output_di
     pge_name: str = pge_config['pge_name']
 
     if pge_name == 'Product_Update':
-        logger.info(f'TEMP: runconfig: {json.dumps(runconfig, indent=2)}')
+        logger.info(f'TEMP: context: {json.dumps(context, indent=2)}')
         shutil.copytree(
             runconfig['input_product_group']['input_product'],
             os.path.join(
