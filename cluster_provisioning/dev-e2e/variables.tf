f6219313f536c773b6feba0e8a5e4c7375a0ab29
Scott Collins <collinss@jpl.nasa.gov>
2025-10-09 10:42:06 -0700

    Rolled DSWx-HLS PGE version to 1.0.4 in Terraform scripts

--- cluster_provisioning/dev-e2e/variables.tf
+++ cluster_provisioning/dev-e2e/variables.tf
@@ -367,7 +367,7 @@ variable "pge_snapshots_date" {
 variable "pge_releases" {
   type = map(string)
   default = {
-    "dswx_hls" = "1.0.3"
+    "dswx_hls" = "1.0.4"
     "cslc_s1"  = "2.1.3"
     "rtc_s1"   = "2.1.3"
     "dswx_s1"  = "3.0.3-dswx-s1"
