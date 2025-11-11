b3ae105edfe883d7401b39ec5ba737d9d69ef85a
Luca Verweyen <lverweye@calpoly.edu>
2025-11-11 10:39:02 -0800

    Remove IRSA from Terraform

--- airflow/terraform/cloud/main.tf
+++ airflow/terraform/cloud/main.tf
@@ -5,7 +5,12 @@ module "airflow" {
 
 module "karpenter" {
   source = "./karpenter"
-  cluster_name = var.cluster_name
-  cluster_endpoint =  var.cluster_endpoint
+  
+  cluster_name     = var.cluster_name
+  cluster_endpoint = var.cluster_endpoint
+  karpenter_irsa_role_arn     = "arn:aws:iam::681612454726:role/am-opera-dev-karpenter-controller"
+  node_role_name              = "am-opera-dev-eks-cluster-worker"
+  node_instance_profile_arn   = "arn:aws:iam::681612454726:instance-profile/am-opera-dev-eks-cluster-worker"
+  security_group_id = "sg-06cd603fe182999ae"
 }
 
