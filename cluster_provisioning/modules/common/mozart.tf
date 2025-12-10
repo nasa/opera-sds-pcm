418591d9d0fd20c4f7a5c8531ba5bc5272df6a70
chrisjrd <chris.jrd@gmail.com>
2025-12-09 16:10:07 -0800

    fix(DSWx-S1): updated integration tests. speed up builds.

--- cluster_provisioning/modules/common/mozart.tf
+++ cluster_provisioning/modules/common/mozart.tf
@@ -688,7 +688,10 @@ resource "aws_instance" "mozart" {
       set -ex
       source ~/.bash_profile
       %{for pge_name, pge_version in var.pge_releases~}
-      if [[ \"${pge_version}\" == \"develop\"* ]]; then
+      cat > /tmp/deploy_${pge_name}.sh << 'SCRIPT'
+      #!/bin/bash
+      source ~/.bash_profile
+      if [[ "${pge_version}" == "develop"* ]]; then
           python ~/mozart/ops/opera-pcm/tools/deploy_pges.py \
           --image_names opera_pge-${pge_name} \
           --pge_release ${pge_version} \
@@ -709,7 +712,16 @@ resource "aws_instance" "mozart" {
           --username ${var.artifactory_fn_user} \
           --api_key ${var.artifactory_fn_api_key}
       fi
+      SCRIPT
+      chmod +x /tmp/deploy_${pge_name}.sh
       %{endfor~}
+
+      # Run all in parallel with xargs
+      ls /tmp/deploy_*.sh | xargs --max-procs=2 -I {} bash {}
+
+      # Cleanup
+      rm -f /tmp/deploy_*.sh
+
       sds -d kibana import -f
       sds -d cloud storage ship_style --bucket ${local.dataset_bucket}
       sds -d cloud storage ship_style --bucket ${local.osl_bucket}
