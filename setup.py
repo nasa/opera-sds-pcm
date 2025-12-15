7e6c6bd86f40b9a5de3b4e66e50e253cfd757c7c
hyunlee <Hyun.H.Lee@jpl.nasa.gov>
2025-12-15 14:04:47 -0800

    Pin boto3-stubs==1.42.5

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
             "validators",
             "cachetools==5.2.0",
 
-            "boto3-stubs",
+            "boto3-stubs==1.42.5",
             "boto3-stubs-lite[essential]==1.42.5",  # for ec2, s3, rds, lambda, sqs, dynamo and cloudformation
 
             "aws-requests-auth",
