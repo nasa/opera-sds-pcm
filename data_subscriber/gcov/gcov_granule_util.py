afd4b45c607c782f06012e8034cce4247908f1aa
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-02-27 12:53:45 -0800

    update gcov utils
    accept literal strings as input

--- data_subscriber/gcov/gcov_granule_util.py
+++ data_subscriber/gcov/gcov_granule_util.py
@@ -13,44 +13,56 @@ def extract_frames_and_track_ids_from_granules(granules):
     """
     return set((extract_frame_id(granule), extract_track_id(granule)) for granule in granules)     
 
+
 def extract_frame_id(granule):
     """
     Extract the frame ID from a granule.
     
     Args:
-        granule: Granule dictionary from data_subscriber.cmr.response_jsons_to_cmr_granules
+        granule: Granule ID string or granule dictionary from data_subscriber.cmr.response_jsons_to_cmr_granules
         
     Returns:
         int: Frame ID
     """
     # This might be in the granule_id or in some metadata field
-    granule_id = granule["granule_id"]
+    if isinstance(granule, dict):
+        granule_id = granule["granule_id"]
+    else:
+        granule_id = granule
     return int(granule_id.split("_")[7])
+
 
 def extract_track_id(granule):
     """
     Extract the track ID from a granule.
     
     Args:
-        granule: Granule dictionary from data_subscriber.cmr.response_jsons_to_cmr_granules
+        granule: Granule ID string or granule dictionary from data_subscriber.cmr.response_jsons_to_cmr_granules
         
     Returns:
         int: Track ID
     """
     # This might be in the granule_id or in some metadata field
-    granule_id = granule["granule_id"]
+    if isinstance(granule, dict):
+        granule_id = granule["granule_id"]
+    else:
+        granule_id = granule
     return int(granule_id.split("_")[5])
+
 
 def extract_cycle_number(granule):
     """
     Extract the cycle number from a granule.
     
     Args:
-        granule: Granule dictionary from data_subscriber.cmr.response_jsons_to_cmr_granules
+        granule: Granule ID string or granule dictionary from data_subscriber.cmr.response_jsons_to_cmr_granules
         
     Returns:
         int: Cycle number
     """
     # This might be in the granule_id or in some metadata field
-    granule_id = granule["granule_id"]
+    if isinstance(granule, dict):
+        granule_id = granule["granule_id"]
+    else:
+        granule_id = granule
     return int(granule_id.split("_")[4])
