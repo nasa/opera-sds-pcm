af5a6433da83b7e678efb24b8eb7a888d822c62f
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-02-27 12:54:23 -0800

    add new gcov utils
    orbit direction and acquisition time range

--- data_subscriber/gcov/gcov_granule_util.py
+++ data_subscriber/gcov/gcov_granule_util.py
@@ -1,6 +1,11 @@
 """
 Provides utility functions for extracting fields from GCOV granules.
 """
+
+from datetime import datetime
+from typing import Tuple
+
+
 def extract_frames_and_track_ids_from_granules(granules):
     """
     Extract frame numbers and track IDs from a list of granules.
@@ -66,3 +71,46 @@ def extract_cycle_number(granule):
     else:
         granule_id = granule
     return int(granule_id.split("_")[4])
+
+
+def extract_orbit_direction(granule) -> str:
+    """
+    Extract the orbit direction from a granule.
+
+    Args:
+        granule: Granule ID string or granule dictionary from data_subscriber.cmr.response_jsons_to_cmr_granules
+
+    Returns:
+        str: Orbit direction ('A' or 'D')
+    """
+    # This might be in the granule_id or in some metadata field
+    if isinstance(granule, dict):
+        granule_id = granule["granule_id"]
+    else:
+        granule_id = granule
+    return str(granule_id.split("_")[6])
+
+
+def extract_acquisition_time_range(granule) -> Tuple[datetime, datetime]:
+    """
+    Extract the acquisition start and end time from a granule.
+
+    Args:
+        granule: Granule ID string or granule dictionary from data_subscriber.cmr.response_jsons_to_cmr_granules
+
+    Returns:
+        Tuple[datetime, datetime]: Acquisition start and end time
+    """
+    # This might be in the granule_id or in some metadata field
+    if isinstance(granule, dict):
+        granule_id = granule["granule_id"]
+    else:
+        granule_id = granule
+
+    granule_fields = granule_id.split("_")
+
+    start = granule_fields[11]
+    end = granule_fields[12]
+
+    return datetime.strptime(start, "%Y%m%dT%H%M%S"), datetime.strptime(end, "%Y%m%dT%H%M%S")
+
