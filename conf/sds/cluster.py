ac20b2fa08fdebeb938ab91733841216916d5c66
pymonger <pymonger@gmail.com>
2026-03-12 16:50:50 -0700

    Add OpenSearch templates for CSC/KSC state-configs and fix missing grq alias on CCSLC

--- conf/sds/cluster.py
+++ conf/sds/cluster.py
@@ -425,6 +425,8 @@ def create_os_index_templates_grq():
         ("os_template_k_cslc_catalog.json",                 "k_cslc_catalog_template"),
         ("os_template_cslc_compressed_product.json",        "cslc_compressed_product_template"),
         ("os_template_rtc_for_dist_catalog.json",           "rtc_for_dist_catalog_template"),
+        ("os_template_cslc_s1_cycle_state_config.json",     "cslc_s1_cycle_state_config_template"),
+        ("os_template_disp_s1_kcycle_state_config.json",    "disp_s1_kcycle_state_config_template"),
     ]:
         copy(
             f"~/.sds/files/opensearch/grq_os_templates/{file}",
