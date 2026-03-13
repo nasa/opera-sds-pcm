2102dc1eab5d369033db761a541e741c1f998e61
pymonger <pymonger@gmail.com>
2026-03-13 08:31:33 -0700

    Add dedicated evaluator ASG and move evaluator jobs off factotum

--- cluster_provisioning/modules/common/variables.tf
+++ cluster_provisioning/modules/common/variables.tf
@@ -359,6 +359,18 @@ variable "queues" {
       "total_jobs_metric" = true
       "use_on_demand"     = false
     }
+    "opera-job_worker-evaluator" = {
+      "name"              = "opera-job_worker-evaluator"
+      "log_file_name"     = "run_evaluator"
+      "instance_type"     = ["c7i.large", "c7a.large", "c6a.large", "c6i.large"]
+      "user_data"         = "launch_template_user_data.sh.tmpl"
+      "root_dev_size"     = 50
+      "data_dev_size"     = 25
+      "min_size"          = 0
+      "max_size"          = 10
+      "total_jobs_metric" = true
+      "use_on_demand"     = false
+    }
     "opera-job_worker-sciflo-l3_dswx_ni" = {
       "name"              = "opera-job_worker-sciflo-l3_dswx_ni"
       "log_file_name"     = "run_sciflo_L3_DSWx_NI"
