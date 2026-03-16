988ccabf835660f45f1de5c5b4c117c0be6f7db4
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-03-16 09:47:39 -0700

    update debug line

--- data_subscriber/rtc_for_dist/rtc_for_dist_query.py
+++ data_subscriber/rtc_for_dist/rtc_for_dist_query.py
@@ -393,7 +393,7 @@ You should update the cmr_rtc_cache using tools/populate_cmr_rtc_cache.py first.
     def download_job_submission_handler(self, total_granules, query_timerange):
 
         def add_filtered_urls(granule, filtered_urls: list, polarization_preference: Union[set, None] =None):
-            self.logger.info(f'add_filtered_urls:: {polarization_preference=})')
+            self.logger.info(f'add_filtered_urls:: {polarization_preference=} {granule["granule_id"]}')
             if granule.get("filtered_urls"):
                 # ignore single polarizations. declared polarization
                 if len(granule.get("polarization", [])) == 1:
