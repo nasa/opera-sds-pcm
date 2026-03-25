36c8cd5033da74cc6958c34294feb71d47bdc849
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-03-25 11:33:29 -0700

    [DISP-S1 3.0.10] version bumps and settings updates

--- cluster_provisioning/ebs-snapshot/variables.tf
+++ cluster_provisioning/ebs-snapshot/variables.tf
@@ -40,7 +40,7 @@ variable "pge_releases" {
     "cslc_s1"  = "2.1.3"
     "rtc_s1"   = "2.1.3"
     "dswx_s1"  = "3.0.4"
-    "disp_s1"  = "3.0.9"
+    "disp_s1"  = "3.0.10"
     "dswx_ni"  = "4.0.0-er.3.0"
     "dist_s1"  = "6.0.0"
     "tropo"    = "3.0.0-rc.1.0-tropo"
