7a464cd0198579cd13d32108d521eb1e581e95b3
chrisjrd <chris.jrd@gmail.com>
2026-04-09 17:08:21 -0700

    feat(DSWx-S1): skip water data

--- data_subscriber/rtc/mgrs_bursts_collection_db_client.py
+++ data_subscriber/rtc/mgrs_bursts_collection_db_client.py
@@ -31,20 +31,20 @@ def dicts(t):
 
 
 @cache
-def cached_load_mgrs_burst_db(filter_land=True):
+def cached_load_mgrs_burst_db(filter_land=True) -> GeoDataFrame:
     """see :func:`~data_subscriber.rtc.mgrs_bursts_collection_db_client.load_mgrs_burst_db`"""
     logger = get_logger()
     logger.info(f"Cache loading MGRS burst database.")
     logger.debug(f"{filter_land=}")
-    return load_mgrs_burst_db(filter_land)
+    return _load_mgrs_burst_db(filter_land)
 
 
-def load_mgrs_burst_db(filter_land=True):
+def _load_mgrs_burst_db(filter_land=True) -> GeoDataFrame:
     """see :func:`~data_subscriber.rtc.mgrs_bursts_collection_db_client.load_mgrs_burst_db_raw`"""
     logger = get_logger()
     logger.info(f"Initial load of MGRS burst database from disk.")
 
-    vector_gdf = load_mgrs_burst_db_raw(filter_land)
+    vector_gdf = _load_mgrs_burst_db_raw(filter_land)
 
     # parse collection columns encoded as string to collections
     vector_gdf["bursts_parsed"] = vector_gdf["bursts"].apply(lambda it: set(ast.literal_eval(it))).values  # downcast to safeguard against index order issues
@@ -55,7 +55,7 @@ def load_mgrs_burst_db(filter_land=True):
     return vector_gdf
 
 
-def load_mgrs_burst_db_raw(filter_land=True) -> GeoDataFrame:
+def _load_mgrs_burst_db_raw(filter_land=True) -> GeoDataFrame:
     """Loads the MGRS Tile Collection Database. On AWS environments, this will localize from a known S3 location."""
     logger = get_logger()
     mtc_local_filepath = Path(os.environ.get("MGRS_TILE_COLLECTION_DB_FILEPATH", "~/Downloads/MGRS_tile_collection_v0.3.sqlite")).expanduser()
