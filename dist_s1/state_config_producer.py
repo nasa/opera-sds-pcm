097360fe5adcffca8119422ffdd5823183fd3075
chrisjrd <chris.jrd@gmail.com>
2026-04-21 11:44:55 -0700

    feat(DSWx-S1): no keyword query

--- dist_s1/state_config_producer.py
+++ dist_s1/state_config_producer.py
@@ -190,7 +190,7 @@ def state_configs_by_batch_id(batch_id):
     grq_es = get_grq_es()
     body = get_body(match_all=False)
     del body["sort"]  # default sort not applicable for these specialized docs
-    body["query"]["bool"]["must"].append({"term": {"metadata.batch_id.keyword": batch_id}})
+    body["query"]["bool"]["must"].append({"term": {"metadata.batch_id": batch_id}})
     try:
         results = grq_es.search(body=body, index="grq_1.0_dist_s1-state-config")
     except opensearchpy.exceptions.NotFoundError as e:
