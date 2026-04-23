483db37134b9da88b7412af56ef7943a78121bbf
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-04-23 11:21:58 -0700

    Undo cache on cmr_session

--- data_subscriber/cmr.py
+++ data_subscriber/cmr.py
@@ -5,7 +5,6 @@ import re
 from collections import namedtuple
 from datetime import datetime, timedelta
 from enum import Enum
-from functools import cache
 from typing import Iterable, Optional, Literal
 
 import dateutil.parser
@@ -120,7 +119,6 @@ COLLECTION_TO_EXTENSIONS_FILTER_MAP = {
 }
 
 
-@cache  # Add cache wrapper to avoid repeated API calls when getting a token on the fly
 def get_cmr_session(endpoint, settings, get_token=True):
     cmr = settings["DAAC_ENVIRONMENTS"][endpoint]["BASE_URL"]
     edl = settings["DAAC_ENVIRONMENTS"][endpoint]["EARTHDATA_LOGIN"]
