c81aa39833a66e58e69ce89f8bb2a2e0b00f4835
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-05-11 08:38:41 -0700

    Implement search & exclusion for HLS

--- data_subscriber/hls/hls_query.py
+++ data_subscriber/hls/hls_query.py
@@ -8,3 +8,15 @@ class HlsCmrQuery(BaseQuery):
     def update_granule_index(self, granule):
         spatial_catalog_conn = HLSSpatialProductCatalog(self.logger)
         spatial_catalog_conn.process_granule(granule)
+
+    def determine_download_granules(self, granules):
+        filtered_granules = []
+
+        for granule in granules:
+            if len(self.es_conn.get_cataloged_granule_by_granule_id(granule['granule_id'])) == 0:
+                self.logger.info(f'Found new granule {granule["granule_id"]}')  # TODO: -> DEBUG
+                filtered_granules.append(granule)
+            else:
+                self.logger.info(f'Dropping granule {granule["granule_id"]} as it has already been cataloged')
+
+        return filtered_granules
