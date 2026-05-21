e4742a1b4a2d0ad83ab720e54f22e814b482e237
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-05-21 11:58:33 -0700

    [RTC-S1 2.1.4] Terraform updates

--- cluster_provisioning/dev/variables.tf
+++ cluster_provisioning/dev/variables.tf
@@ -374,7 +374,7 @@ variable "pge_releases" {
   default = {
     "dswx_hls" = "1.0.4"
     "cslc_s1"  = "2.1.3"
-    "rtc_s1"   = "2.1.3"
+    "rtc_s1"   = "2.1.4"
     "dswx_s1"  = "3.0.4"
     "disp_s1"  = "3.0.10"
     "dswx_ni"  = "4.0.0-rc.1.0"
