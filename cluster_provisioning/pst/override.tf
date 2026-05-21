6bf3affb46a46371b980767ded9b403159bfacc9
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-05-21 13:54:10 -0700

    fix terraform validation block

--- cluster_provisioning/pst/override.tf
+++ cluster_provisioning/pst/override.tf
@@ -243,5 +243,6 @@ variable "es_snapshot_destroy_action" {
 
   validation {
     condition = contains(["leave", "purge", "create-new"], var.es_snapshot_destroy_action)
+    error_message = "The value of es_snapshot_destroy_action must be one of \"leave\", \"purge\", \"create-new\"."
   }
 }
