33a4d9f683f6086f2d316e32f8dd1a353da31f1a
Riley Kuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-05-28 14:52:21 -0700

    point cluster overrides to 6.0.3 release

--- cluster_provisioning/int_pst_ops_devcommon_override.tf
+++ cluster_provisioning/int_pst_ops_devcommon_override.tf
@@ -6,25 +6,25 @@ variable "hysds_release" {
 }
 
 variable "lambda_package_release" {
-  default = "6.0.2"
+  default = "6.0.3"
 }
 
 variable "pcm_commons_branch" {
-  default = "6.0.2"
+  default = "6.0.3"
 }
 
 variable "pcm_branch" {
-  default = "6.0.2"
+  default = "6.0.3"
 }
 
 variable "product_delivery_branch" {
-  default = "6.0.2"
+  default = "6.0.3"
 }
 
 variable "bach_api_branch" {
-  default = "6.0.2"
+  default = "6.0.3"
 }
 
 variable "bach_ui_branch" {
-  default = "6.0.2"
+  default = "6.0.3"
 }
