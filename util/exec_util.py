e72fddad8a6f1b268e25f803b0013b3f2f446a9d
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-06-01 08:08:14 -0700

    call_noerr exception

--- util/exec_util.py
+++ util/exec_util.py
@@ -66,7 +66,9 @@ def call_noerr(cmd, work_dir, logr=logger):
         info_dict["stderr"] = e.output.decode()
         logr.critical("Got exception running:\n{}\nSTDOUT/STDERR:\n{}".format(cmd, e.output.decode()))
         # raise RuntimeError(e.output.decode())
-        raise RuntimeError().add_note(e.output.decode())
+        err = RuntimeError()
+        err.add_note(e.output.decode())
+        raise err
         # raise e.with_traceback(e.output.decode())
     except Exception as e:
         logr.error("Got exception running:\n{}\nException: {}".format(cmd, str(e)))
