d3032aa33baad0884d1d93b97356e81aafbc96d6
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-06-03 11:50:37 -0700

    remove 64 GiB inst types from dswx-s1 ASG

--- cluster_provisioning/modules/common/variables.tf
+++ cluster_provisioning/modules/common/variables.tf
@@ -323,7 +323,7 @@ variable "queues" {
       "name"          = "opera-job_worker-sciflo-l3_dswx_s1"
       "log_file_name" = "run_sciflo_L3_DSWx_S1"
       "instance_type" = ["c6i.2xlarge", "c7i.2xlarge", "c8i.2xlarge", "c6i.4xlarge", "c7i.4xlarge", "c8i.4xlarge",
-      "m6i.2xlarge", "m7i.2xlarge", "m8i.2xlarge", "m6i.4xlarge", "m7i.4xlarge", "m8i.4xlarge"]
+      "m6i.2xlarge", "m7i.2xlarge", "m8i.2xlarge"]
       "user_data"         = "launch_template_user_data.sh.tmpl"
       "root_dev_size"     = 50
       "data_dev_size"     = 100
