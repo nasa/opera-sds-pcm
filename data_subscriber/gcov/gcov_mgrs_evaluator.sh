3206ee763afdadcfec24c11bff9918ce71296eff
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-06-23 10:10:30 -0700

    rename log to match asg logfile

--- data_subscriber/gcov/gcov_mgrs_evaluator.sh
+++ data_subscriber/gcov/gcov_mgrs_evaluator.sh
@@ -2,4 +2,4 @@
 export OPERA_HOME=/home/ops/verdi/ops/opera-pcm
 export PYTHONPATH=$OPERA_HOME:$PYTHONPATH
 source $HOME/verdi/bin/activate
-python $OPERA_HOME/data_subscriber/gcov/gcov_mgrs_evaluator.py > run_mgrs_evaluator.log 2>&1
+python $OPERA_HOME/data_subscriber/gcov/gcov_mgrs_evaluator.py > run_evaluator.log 2>&1
