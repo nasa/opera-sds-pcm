b4d8dd8b88082ae97df5af0fe969183d307cbd3c
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-07-01 15:38:49 -0700

    [DISP-NI 6.0.0-RC.1.0] Terraform updates

--- cluster_provisioning/dev-int/override.tf
+++ cluster_provisioning/dev-int/override.tf
@@ -126,7 +126,7 @@ variable "pge_releases" {
     "dswx_ni"  = "4.0.0-rc.2.0"
     "tropo"    = "3.0.0-er.3.0-tropo"
     "dist_s1"  = "6.0.2"
-    "disp_ni"  = "6.0.0-er.2.0"
+    "disp_ni"  = "6.0.0-rc.1.0"
     "cal_disp" = "7.0.0-er.1.0"
   }
 }
