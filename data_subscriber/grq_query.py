a69bed9212ca0fa4ab30acf87c6ba1d262391a1a
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-08-28 11:59:38 -0700

    flesh out native id query

--- data_subscriber/grq_query.py
+++ data_subscriber/grq_query.py
@@ -9,6 +9,9 @@ from opensearchpy.helpers import scan
 from data_subscriber.cmr import Collection, ProductType, COLLECTION_TO_PRODUCT_TYPE_MAP
 from opera_commons.es_connection import get_grq_es
 from opera_commons.logger import get_logger
+from data_subscriber.rtc import mgrs_bursts_collection_db_client as mbc_client
+from rtc_utils import rtc_granule_regex
+
 
 
 logger = get_logger()
@@ -55,6 +58,7 @@ def _build_grq_query(args, timerange: DateTimeRange) -> dict:
     bbox = args.bbox
 
     if bbox != GLOBAL_BBOX:
+        # TODO: Can now implement since we're indexing RTC/CSLC polygons
         raise NotImplementedError('GRQ querying by bbox not yet implemented')
 
     if COLLECTION_TO_PRODUCT_TYPE_MAP[args.collection] not in SUPPORTED_PRODUCT_TYPES:
@@ -74,7 +78,7 @@ def _build_grq_query(args, timerange: DateTimeRange) -> dict:
     }
 
     must = []
-    must_not = []
+    should = []
 
     if args.use_temporal:
         must.append({
@@ -96,16 +100,42 @@ def _build_grq_query(args, timerange: DateTimeRange) -> dict:
         })
 
     if args.native_id is not None:
-        # TODO: Validate keyword + handle wildcards?
-        must.append({
-            "term": {
-                "id": args.native_id
-            }
-        })
+        # TODO: Validate?
+
+        if COLLECTION_TO_PRODUCT_TYPE_MAP[args.collection] == ProductType.RTC:
+            mgrs = mbc_client.cached_load_mgrs_burst_db(filter_land=True)
+            # extract burst ID from the native-ID, and find the 1-2 relevant MGRS burst sets containing that burst ID.
+            match_native_id = re.match(rtc_granule_regex, args.native_id)
+            burst_id = mbc_client.product_burst_id_to_mapping_burst_id(match_native_id.group("burst_id"))
+            native_ids = mbc_client.get_reduced_rtc_native_id_patterns(mgrs[mgrs["bursts"].str.contains(burst_id)])
+
+            if not native_ids:
+                raise ValueError(f"The supplied {args.native_id=} is not associated with "
+                                 f"any land-based MGRS tile collection.")
+
+            for nid in native_ids:
+                should.append({
+                    "wildcard": {
+                        "id.keyword": nid
+                    }
+                })
+        else:
+            if '*' in args.native_id or '?' in args.native_id:
+                must.append({
+                    "wildcard": {
+                        "id.keyword": args.native_id
+                    }
+                })
+            else:
+                must.append({
+                    "term": {
+                        "id.keyword": args.native_id
+                    }
+                })
 
     if len(must) > 0:
         query["query"]["bool"]["must"] = must
-    if len(must_not) > 0:
-        query["query"]["bool"]["must_not"] = must_not
+    if len(should) > 0:
+        query["query"]["bool"]["should"] = should
 
     return query
