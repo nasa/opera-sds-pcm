24f69d7d6fcdae830c60df399553ebf3bb9b95f5
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-09-09 12:23:29 -0700

    bump rtc query instances m.l -> c.xl

--- cluster_provisioning/modules/common/variables.tf
+++ cluster_provisioning/modules/common/variables.tf
@@ -672,7 +672,10 @@ variable "queues" {
     }
     "opera-job_worker-rtc_for_dist_data_query" = {
       "name"              = "opera-job_worker-rtc_for_dist_data_query"
-      "instance_type"     = ["m6i.large", "m6a.large", "m5.large", "m5a.large"]
+      "instance_type"     = ["c5.xlarge", "c5a.xlarge", "c5ad.xlarge", "c5d.xlarge", "c6a.xlarge", "c6g.xlarge", "c6gd.xlarge",
+        "c6gn.xlarge", "c6i.xlarge", "c6id.xlarge", "c6in.xlarge", "c7a.xlarge", "c7g.xlarge", "c7gd.xlarge",
+        "c7gn.xlarge", "c7i-flex.xlarge", "c7i.xlarge", "c8a.xlarge", "c8g.xlarge", "c8gb.xlarge",
+      "c8gd.xlarge", "c8gn.xlarge", "c8i-flex.xlarge", "c8i.xlarge", "c8id.xlarge"]
       "user_data"         = "launch_template_user_data_no_swap.sh.tmpl"
       "root_dev_size"     = 50
       "data_dev_size"     = 25
